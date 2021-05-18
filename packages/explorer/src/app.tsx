8e48280c868ccc74474a6b38057c39415ad57862
Andre Kohn <kohn.a@outlook.com>
2021-05-18 18:15:59 +0200

    Add initial explorer scaffold

---
+++ packages/explorer/src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-resizable/css/styles.css';
+import 'react-virtualized/styles.css';
+
+ReactDOM.render(<div />, document.getElementById('root'));
