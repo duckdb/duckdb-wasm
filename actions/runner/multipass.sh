ae22dd6b09dd5678f933751e2b233d697540d18b
Andre Kohn <kohn.a@outlook.com>
2021-05-18 11:32:28 +0200

    Notes on self-hosting action runner

--- actions/runner/multipass.sh
+++ actions/runner/multipass.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Configure multipass to use the libvirt driver:
+#
+# sudo apt install libvirt-daemon-system
+# sudo snap connect multipass:libvirt
+# sudo multipass set local.driver=libvirt
+
+
 multipass launch \
-    --cpus 4 \
+    --cpus 8 \
     --disk 100G \
     --mem 6G \
-    --name github-actions-runner \
+    --name github-duckdb-wasm-ci \
     --cloud-init ./cloud-init.yaml
 
