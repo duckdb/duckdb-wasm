01f52d47005887291c33a29507d7001ef6654d35
Andre Kohn <kohn.a@outlook.com>
2021-05-18 11:48:09 +0200

    Add setup instructions

--- actions/runner/multipass.sh
+++ actions/runner/multipass.sh
@@ -5,6 +5,11 @@
 # sudo apt install libvirt-daemon-system
 # sudo snap connect multipass:libvirt
 # sudo multipass set local.driver=libvirt
+#
+# Let the cloud-init setup your vm, afterwards do the following steps:
+#   * Su actions and register the runner in /home/actions/runner
+#   * Install the action runner as root via /home/actions/runner/svc.sh install actions
+#   * Providing the user actions to svc.sh install is crucial since the service needs the correct permissions!
 
 
 multipass launch \
