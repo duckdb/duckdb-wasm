0ecdd603876f68c11acfe77d0314e57516261d11
Andre Kohn <kohn.a@outlook.com>
2021-05-19 14:47:24 +0200

    First blocking database init

--- packages/explorer/src/model/state.ts
+++ packages/explorer/src/model/state.ts
@@ -1,13 +1,18 @@
+import * as arrow from 'apache-arrow';
+
 export interface AppState {
     /// The launch is complete?
     script: string;
-    ///
+    /// The tokens within the script for syntax highlighting
     scriptTokens: Array<any>;
+    /// The current result table (if any)
+    queryResult: arrow.Table | null;
 }
 
 export function createDefaultState(): AppState {
     return {
         script: 'foo',
         scriptTokens: [],
+        queryResult: null,
     };
 }
