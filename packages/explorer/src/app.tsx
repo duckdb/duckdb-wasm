228462b901b2a3e45a4787945a0809dee8dacb45
Andre Kohn <kohn.a@outlook.com>
2021-05-19 09:23:48 +0200

    Resolve duckdb worker url

--- packages/explorer/src/app.tsx
+++ packages/explorer/src/app.tsx
@@ -1,13 +1,13 @@
 import { embed } from './embed';
 
-// const worker = new URL('@dashql/duckdb/dist/duckdb-browser-parallel.worker.js', import.meta.url);
+const worker = new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-parallel.worker.js', import.meta.url);
 
 const doc = document.getElementById('root');
 if (!doc) {
     console.error("no element with id 'root' found");
 } else {
     embed(doc!, {
-        workerURL: undefined,
+        workerURL: worker,
         withNavbar: true,
     });
 }
