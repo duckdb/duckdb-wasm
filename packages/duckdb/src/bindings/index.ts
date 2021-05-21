c872780a8e4319dbb33dc574b36a1b498a4bebe0
Andre Kohn <kohn.a@outlook.com>
2021-05-21 11:53:56 +0200

    Prepare json & csv import tests in browsers and node

--- packages/duckdb/src/bindings/index.ts
+++ packages/duckdb/src/bindings/index.ts
@@ -2,3 +2,4 @@ export * from './bindings';
 export * from './connection';
 export * from './runtime_base';
 export * from './duckdb_module';
+export * from './table_options';
---
+++ packages/duckdb/test/import_json.test.ts
@@ -0,0 +1,63 @@
+import * as duckdb from '../src/';
+// import * as arrow from 'apache-arrow';
+
+interface JSONImportTest {
+    name: string;
+    input: string;
+    options: duckdb.JSONTableOptions;
+    query: string;
+    expectedOutput: string;
+}
+
+const JSON_IMPORT_TESTS: JSONImportTest[] = [
+    {
+        name: 'rows_integers',
+        input: `[
+            {"a":1, "b":2, "c":3},
+            {"a":4, "b":5, "c":6},
+            {"a":7, "b":8, "c":9},
+        ]`,
+        options: {
+            schema: 'main',
+            name: 'foo',
+        },
+        query: 'SELECT * FROM main.foo',
+        expectedOutput: '',
+    },
+];
+
+export function testJSONImport(db: () => duckdb.DuckDBBindings): void {
+    let conn: duckdb.DuckDBConnection;
+
+    beforeEach(async () => {
+        conn = db().connect();
+    });
+    afterEach(async () => {
+        conn.disconnect();
+    });
+    describe('JSON Import Tests', () => {
+        for (const test of JSON_IMPORT_TESTS) {
+            it(test.name, () => {
+                expect(null).toBeNull();
+            });
+        }
+    });
+}
+
+export function testJSONImportAsync(db: () => duckdb.AsyncDuckDB): void {
+    let conn: duckdb.AsyncDuckDBConnection;
+
+    beforeEach(async () => {
+        conn = await db().connect();
+    });
+    afterEach(async () => {
+        await conn.disconnect();
+    });
+    describe('JSON Import Tests', () => {
+        for (const test of JSON_IMPORT_TESTS) {
+            it(test.name, async () => {
+                expect(null).toBeNull();
+            });
+        }
+    });
+}
