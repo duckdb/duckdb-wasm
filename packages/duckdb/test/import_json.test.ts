8b5bb9ce7f839e6b26b67a427c56954cff270493
Andre Kohn <kohn.a@outlook.com>
2021-05-21 13:18:56 +0200

    Test json import

--- packages/duckdb/test/import_json.test.ts
+++ packages/duckdb/test/import_json.test.ts
@@ -3,13 +3,17 @@ import * as arrow from 'apache-arrow';
 
 const encoder = new TextEncoder();
 
+interface Column {
+    name: string;
+    values: any[];
+}
+
 interface JSONImportTest {
     name: string;
     input: string;
     options: duckdb.JSONTableOptions;
     query: string;
-    expectedColumnNames: string[];
-    expectedColumnData: any[][];
+    expectedColumns: Column[];
 }
 
 const JSON_IMPORT_TESTS: JSONImportTest[] = [
@@ -25,30 +29,46 @@ const JSON_IMPORT_TESTS: JSONImportTest[] = [
             name: 'foo',
         },
         query: 'SELECT * FROM main.foo',
-        expectedColumnNames: ['a', 'b', 'c'],
-        expectedColumnData: [
-            [1, 4, 7],
-            [2, 5, 8],
-            [3, 6, 9],
+        expectedColumns: [
+            { name: 'a', values: [1, 4, 7] },
+            { name: 'b', values: [2, 5, 8] },
+            { name: 'c', values: [3, 6, 9] },
+        ],
+    },
+    {
+        name: 'cols_integers',
+        input: `{
+            "a": [1, 4, 7],
+            "b": [2, 5, 8],
+            "c": [3, 6, 9]
+        }`,
+        options: {
+            schema: 'main',
+            name: 'foo',
+        },
+        query: 'SELECT * FROM main.foo',
+        expectedColumns: [
+            { name: 'a', values: [1, 4, 7] },
+            { name: 'b', values: [2, 5, 8] },
+            { name: 'c', values: [3, 6, 9] },
         ],
     },
 ];
 
 const PATH = 'TEST';
 
-export function compareTable(table: arrow.Table, expectedColNames: string[], expectedColData: any[][]): void {
+export function compareTable(table: arrow.Table, expected: Column[]): void {
     // Check column count
-    expect(expectedColNames.length).toEqual(expectedColData.length);
-    const colCount = expectedColNames.length;
+    const colCount = expected.length;
     expect(table.numCols).toEqual(colCount);
     if (colCount == 0) return;
 
     // Check columns
-    const rowCount = expectedColData[0].length;
+    const rowCount = expected[0].values.length;
     for (let i = 0; i < colCount; ++i) {
-        expect(expectedColData[i].length).toEqual(rowCount);
+        expect(expected[i].values.length).toEqual(rowCount);
         expect(table.getColumnAt(i)?.length).toEqual(rowCount);
-        expect(table.getColumnAt(i)?.name).toEqual(expectedColNames[i]);
+        expect(table.getColumnAt(i)?.name).toEqual(expected[i].name);
     }
 
     // Compare the actual values
@@ -58,7 +78,7 @@ export function compareTable(table: arrow.Table, expectedColNames: string[], exp
         for (let j = 0; j < rowCount; ++j) {
             have.push(col.get(j));
         }
-        expect(have).toEqual(expectedColData[i]);
+        expect(have).toEqual(expected[i].values);
     }
 }
 
@@ -76,11 +96,11 @@ export function testJSONImport(db: () => duckdb.DuckDBBindings): void {
         for (const test of JSON_IMPORT_TESTS) {
             it(test.name, () => {
                 const buffer = encoder.encode(test.input);
+                conn.runQuery(`DROP TABLE IF EXISTS ${test.options.schema || 'main'}.${test.options.name}`);
                 db().addFileBuffer(PATH, buffer);
                 conn.importJSONFromPath(PATH, test.options);
-
                 const results = conn.runQuery(test.query);
-                compareTable(results, test.expectedColumnNames, test.expectedColumnData);
+                compareTable(results, test.expectedColumns);
             });
         }
     });
@@ -100,11 +120,11 @@ export function testJSONImportAsync(db: () => duckdb.AsyncDuckDB): void {
         for (const test of JSON_IMPORT_TESTS) {
             it(test.name, async () => {
                 const buffer = encoder.encode(test.input);
+                await conn.runQuery(`DROP TABLE IF EXISTS ${test.options.schema || 'main'}.${test.options.name}`);
                 await db().addFileBuffer(PATH, buffer);
                 await conn.importJSONFromPath(PATH, test.options);
-
                 const results = await conn.runQuery(test.query);
-                compareTable(results, test.expectedColumnNames, test.expectedColumnData);
+                compareTable(results, test.expectedColumns);
             });
         }
     });
