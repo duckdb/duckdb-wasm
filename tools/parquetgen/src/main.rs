620a5457a83c444b002a2a609b8298f54531ea50
Andre Kohn <kohn.a@outlook.com>
2021-05-22 13:12:53 +0200

    Tweak parquet generator

--- tools/parquetgen/src/main.rs
+++ tools/parquetgen/src/main.rs
@@ -1,16 +1,69 @@
-use std::path::Path;
-use std::{env, process};
+use clap::{self, ArgMatches};
+use clap::{App, Arg};
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process;
 
 mod error;
+mod tpch;
 mod uni;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: pkg_uni_schema <dir>");
-        process::exit(1);
-    }
-    let out_dir = Path::new(&args[1]);
+    let matches = App::new("Parquet Generator")
+        .version("0.1")
+        .author("Andre Kohn. <kohn.a@outlook.com>")
+        .subcommand(
+            App::new("uni")
+                .about("Generates parquet files for the university schema")
+                .arg(
+                    Arg::new("out")
+                        .short('o')
+                        .takes_value(true)
+                        .required(true)
+                        .about("Directory where the parquet files are written"),
+                ),
+        )
+        .subcommand(
+            App::new("tpch")
+                .about("Generates parquet files for the university schema")
+                .arg(
+                    Arg::new("out")
+                        .short('o')
+                        .takes_value(true)
+                        .required(true)
+                        .about("Directory where the parquet files are written"),
+                )
+                .arg(
+                    Arg::new("in")
+                        .short('i')
+                        .takes_value(true)
+                        .required(true)
+                        .about("Directory with generated tbl files"),
+                ),
+        )
+        .get_matches();
 
-    uni::write_tables(&out_dir)
+    let require_dir_arg = |matches: &ArgMatches, arg_name: &str| {
+        let arg = matches.value_of(arg_name).unwrap();
+        let arg_path = Path::new(&arg);
+        let metadata = fs::metadata(&arg_path);
+        if metadata.is_err() || !metadata.unwrap().is_dir() {
+            println!("Invalid output directory: {}", arg);
+            process::exit(1);
+        }
+        return PathBuf::from(arg_path);
+    };
+
+    if let Some(ref matches) = matches.subcommand_matches("uni") {
+        let out_dir = require_dir_arg(&matches, "out");
+        uni::write_tables(&out_dir);
+        process::exit(0);
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("uni") {
+        let out_dir = require_dir_arg(&matches, "out");
+        let in_dir = require_dir_arg(&matches, "in");
+        tpch::convert_tbls(&in_dir, &out_dir);
+        process::exit(0);
+    }
 }
