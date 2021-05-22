cb8fd9661902e7f1dfdde89a5cf5ec2e6d67acc2
Andre Kohn <kohn.a@outlook.com>
2021-05-22 12:40:30 +0200

    Work on parquet generator

---
+++ tools/parquetgen/src/error.rs
@@ -0,0 +1,27 @@
+use std::error;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Error {
+    message: String,
+}
+
+impl Error {
+    pub fn new(msg: String) -> Box<Self> {
+        Box::new(Self {
+            message: msg.to_string(),
+        })
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
