e49b14456bbbc398cf945795abd70b16740ec266
Andre Kohn <kohn.a@outlook.com>
2021-05-22 15:47:57 +0200

    Work on tpch parquet writer

--- tools/parquetgen/src/tpch.rs
+++ tools/parquetgen/src/tpch.rs
@@ -4,23 +4,36 @@ use parquet::arrow::arrow_to_parquet_schema;
 use std::fs;
 use std::path::PathBuf;
 use std::process;
+use std::sync::Arc;
 
 fn convert_tbl(
-    tbl_file: PathBuf,
-    schema: Schema,
-    _out_dir: &PathBuf,
+    tbl_file_path: PathBuf,
+    out_file_path: PathBuf,
+    schema: Arc<Schema>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let tbl_md = fs::metadata(&tbl_file);
+    let tbl_md = fs::metadata(&tbl_file_path);
     if tbl_md.is_err() || !tbl_md.unwrap().is_dir() {
-        println!("Tbl does not exist: {}", tbl_file.to_str().unwrap());
+        println!("Tbl does not exist: {}", tbl_file_path.to_str().unwrap());
         process::exit(1);
     }
     let _parquet_schema = arrow_to_parquet_schema(&schema)?;
 
     // Create csv reader
+    let tbl_file = fs::File::open(tbl_file_path)?;
+    let reader = arrow::csv::ReaderBuilder::new()
+        .with_delimiter('|' as u8)
+        .with_schema(schema.clone())
+        .with_batch_size(1024)
+        .build(tbl_file)?;
 
     // Stream to parquet writer
+    let parquet_file = fs::File::create(out_file_path)?;
+    let mut writer = parquet::arrow::ArrowWriter::try_new(parquet_file, schema.clone(), None)?;
 
+    // Write all batches
+    for batch in reader {
+        writer.write(&batch?)?;
+    }
     return Ok(());
 }
 
@@ -30,7 +43,8 @@ pub fn convert_tbls(
 ) -> Result<(), Box<dyn std::error::Error>> {
     convert_tbl(
         tbl_dir.join("lineitem.tbl"),
-        Schema::new(vec![
+        out_dir.join("lineitem.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("l_orderkey", DataType::Int32, false),
             Field::new("l_partkey", DataType::Int32, false),
             Field::new("l_suppkey", DataType::Int32, false),
@@ -59,12 +73,12 @@ pub fn convert_tbls(
             Field::new("l_shipinstruct", DataType::Utf8, false),
             Field::new("l_shipmode", DataType::Utf8, false),
             Field::new("l_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("supplier.tbl"),
-        Schema::new(vec![
+        out_dir.join("supplier.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("s_suppkey", DataType::Int32, false),
             Field::new("s_name", DataType::Utf8, false),
             Field::new("s_address", DataType::Utf8, false),
@@ -72,12 +86,12 @@ pub fn convert_tbls(
             Field::new("s_phone", DataType::Utf8, false),
             Field::new("s_acctbal", DataType::Decimal(12, 2), false),
             Field::new("s_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("customer.tbl"),
-        Schema::new(vec![
+        out_dir.join("customer.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("c_custkey", DataType::Int32, false),
             Field::new("c_name", DataType::Utf8, false),
             Field::new("c_address", DataType::Utf8, false),
@@ -86,12 +100,12 @@ pub fn convert_tbls(
             Field::new("c_acctbal", DataType::Decimal(12, 2), false),
             Field::new("c_mktsegment", DataType::Utf8, false),
             Field::new("c_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("part.tbl"),
-        Schema::new(vec![
+        out_dir.join("part.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("p_partkey", DataType::Int32, false),
             Field::new("p_name", DataType::Utf8, false),
             Field::new("p_mfgr", DataType::Utf8, false),
@@ -101,38 +115,37 @@ pub fn convert_tbls(
             Field::new("p_container", DataType::Utf8, false),
             Field::new("p_retailprice", DataType::Decimal(12, 2), false),
             Field::new("p_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("partsupp.tbl"),
-        Schema::new(vec![
+        out_dir.join("partsupp.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("ps_partkey", DataType::Int32, false),
             Field::new("ps_suppkey", DataType::Int32, false),
             Field::new("ps_availqty", DataType::Int32, false),
             Field::new("ps_supplycost", DataType::Decimal(12, 2), false),
             Field::new("ps_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("region.tbl"),
-        Schema::new(vec![
+        out_dir.join("region.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("r_regionkey", DataType::Int32, false),
             Field::new("r_name", DataType::Utf8, false),
             Field::new("r_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     convert_tbl(
         tbl_dir.join("nation.tbl"),
-        Schema::new(vec![
+        out_dir.join("nation.parquet"),
+        Arc::new(Schema::new(vec![
             Field::new("n_nationkey", DataType::Int32, false),
             Field::new("n_name", DataType::Utf8, false),
             Field::new("n_regionkey", DataType::Int32, false),
             Field::new("n_comment", DataType::Utf8, false),
-        ]),
-        out_dir,
+        ])),
     )?;
     return Ok(());
 }
