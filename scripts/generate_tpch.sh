87cefc50735144d2e0cf51f1dad64fd02b90770c
Andre Kohn <kohn.a@outlook.com>
2021-05-23 23:21:00 +0200

    Fix tpch generation on MacOS

--- scripts/generate_tpch.sh
+++ scripts/generate_tpch.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 trap exit SIGINT
 
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
-DBGEN_DIR="${PROJECT_ROOT}/submodules/tpch-dbgen"
+DBGEN_DIR="${PROJECT_ROOT}/submodules/tpch-dbgen/dbgen"
 DBGEN=${DBGEN_DIR}/dbgen
 PARQUETGEN=${PROJECT_ROOT}/target/release/parquetgen
 
@@ -20,6 +20,14 @@ TPCH_SF_OUT_PARQUET=${TPCH_SF_OUT}/parquet
 echo "SCALE_FACTOR=${SCALE_FACTOR}"
 
 if [ ! -f ${DBGEN} ]; then
+    cd ${DBGEN_DIR}
+    case "$(uname)" in
+        'Linux');;
+        'Darwin')
+            echo "Patch Makefile"
+            sed -i '.bak' -e "s/LINUX/MACOS/g" ./Makefile
+            ;; 
+    esac
     make -C ${DBGEN_DIR} dbgen
 fi
 echo "DBGEN=${DBGEN}"
@@ -29,18 +37,17 @@ if [ ! -f ${PARQUETGEN} ]; then
 fi
 echo "PARQUETGEN=${PARQUETGEN}"
 
-TBL_COUNT=$(ls -1 ${TPCH_SF_OUT_TBL}/*.tbl 2>/dev/null | wc -l)
+mkdir -p ${TPCH_SF_OUT_TBL}
+TBL_COUNT=$(find ${TPCH_SF_OUT_TBL} -name "*.tbl" | wc -l)
 if [[ ${TBL_COUNT} -ne 8 ]]; then
-    mkdir -p ${TPCH_SF_OUT_TBL}
-    cd ${DBGEN_DIR}
     DSS_PATH=${TPCH_SF_OUT_TBL} ./dbgen -vf -s ${SCALE_FACTOR}
 fi
 echo "TPCH_SF_OUT_TBL=${TPCH_SF_OUT_TBL}"
 
-PARQUET_COUNT=$(ls -1 ${TPCH_SF_OUT_PARQUET}/*.parquet 2>/dev/null | wc -l)
+mkdir -p ${TPCH_SF_OUT_PARQUET}
+PARQUET_COUNT=$(find ${TPCH_SF_OUT_TBL} -name "*.parquet" | wc -l)
 if [[ ${PARQUET_COUNT} -ne 8 ]]; then
-    rm -rf ${TPCH_SF_OUT_PARQUET}
-    mkdir -p ${TPCH_SF_OUT_PARQUET}
+    rm -rf ${TPCH_SF_OUT_PARQUET}/*.parquet
     ${PARQUETGEN} tpch -i ${TPCH_SF_OUT_TBL} -o ${TPCH_SF_OUT_PARQUET}
 fi
 echo "TPCH_SF_OUT_PARQUET=${TPCH_SF_OUT_PARQUET}"
