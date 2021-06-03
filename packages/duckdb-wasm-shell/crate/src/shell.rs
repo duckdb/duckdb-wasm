c7fcada04368886d50957d1a278b73a850833d6a
Andre Kohn <kohn.a@outlook.com>
2021-06-03 14:36:23 +0200

    Work on shell

--- packages/duckdb-wasm-shell/crate/src/shell.rs
+++ packages/duckdb-wasm-shell/crate/src/shell.rs
@@ -1,26 +1,123 @@
+use crate::term_codes;
+use crate::xterm::{OnKeyEvent, Terminal};
 use std::cell::RefCell;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
-use crate::xterm::Terminal;
+const PROMPT: &str = "duckdb> ";
 
 thread_local! {
-    static SHELL: RefCell<Shell> = RefCell::new(Shell::new());
+    static SHELL: RefCell<Shell> = RefCell::new(Shell::default());
 }
 
+/// The shell is the primary entrypoint for the Javascript api.
+/// It is stored as thread_local singleton and maintains all the state for the interactions with DuckDB
 pub struct Shell {
+    /// The actual xterm terminal instance
     terminal: Terminal,
+    /// The current line buffer
+    line: String,
+    /// The cursor column
+    cursor_column: usize,
 }
 
 impl Shell {
     /// Construct a shell
-    fn new() -> Self {
+    fn default() -> Self {
         Self {
             terminal: Terminal::construct(None),
+            line: String::new(),
+            cursor_column: 0,
+        }
+    }
+
+    /// Process on-key event
+    pub fn on_key(&mut self, e: OnKeyEvent) {
+        let event = e.dom_event();
+        match event.key_code() {
+            term_codes::KEY_ENTER => {
+                if !self.line.is_empty() {
+                    self.terminal.writeln("");
+                    self.terminal.writeln(&format!(
+                        "You entered {} characters '{}'",
+                        self.line.len(),
+                        self.line
+                    ));
+                    self.line.clear();
+                    self.cursor_column = 0;
+                }
+                self.write_prompt();
+            }
+            term_codes::KEY_BACKSPACE => {
+                if self.cursor_column > 0 {
+                    self.terminal.write("\u{0008} \u{0008}");
+                    self.line.pop();
+                    self.cursor_column -= 1;
+                }
+            }
+            term_codes::KEY_LEFT_ARROW => {
+                if self.cursor_column > 0 {
+                    self.terminal.write(term_codes::CURSOR_LEFT);
+                    self.cursor_column -= 1;
+                }
+            }
+            term_codes::KEY_RIGHT_ARROW => {
+                if self.cursor_column < self.line.len() {
+                    self.terminal.write(term_codes::CURSOR_RIGHT);
+                    self.cursor_column += 1;
+                }
+            }
+            term_codes::KEY_L if event.ctrl_key() => self.terminal.clear(),
+            term_codes::KEY_C if event.ctrl_key() => {
+                self.write_prompt();
+                self.line.clear();
+                self.cursor_column = 0;
+            }
+            _ => {
+                if !event.alt_key() && !event.alt_key() && !event.ctrl_key() && !event.meta_key() {
+                    self.terminal.write(&event.key());
+                    self.line.push_str(&e.key());
+                    self.cursor_column += 1;
+                }
+            }
         }
     }
 
     /// Attach to a terminal
     pub fn attach(&mut self, term: Terminal) {
         self.terminal = term;
+
+        // Register on_key callback
+        let callback = Closure::wrap(Box::new(move |e: OnKeyEvent| {
+            Shell::global_mut(|s| s.on_key(e));
+        }) as Box<dyn FnMut(_)>);
+        self.terminal.on_key(callback.as_ref().unchecked_ref());
+        callback.forget();
+    }
+
+    /// Write the DuckDB logo
+    pub fn write_logo(&self) {
+        self.terminal.writeln("   ▄▄███▄▄           ");
+        self.terminal.writeln(" ███████████         ");
+        self.terminal.writeln("█████████████  ███▄  ");
+        self.terminal.writeln("█████████████  ███▀  ");
+        self.terminal.writeln(" ███████████         ");
+        self.terminal.writeln("   ▀▀███▀▀           ");
+    }
+
+    /// Write a prompt
+    pub fn write_prompt(&self) {
+        self.terminal.writeln("");
+        self.terminal.write(PROMPT);
+    }
+
+    /// Write the initial greeter
+    pub fn write_greeter(&self) {
+        self.terminal.write(PROMPT);
+    }
+
+    pub fn focus(&self) {
+        self.terminal.focus();
     }
 
     /// Access the global shell object
--- packages/duckdb-wasm-shell/crate/src/shell_embedding.rs
+++ packages/duckdb-wasm-shell/crate/src/shell_embedding.rs
@@ -1,10 +1,8 @@
 use crate::shell;
 use crate::shell_options;
-use crate::term_codes;
-use crate::term_printing;
 use crate::xterm::addons::fit::FitAddon;
 use crate::xterm::addons::webgl::WebglAddon;
-use crate::xterm::{OnKeyEvent, Terminal, TerminalOptions, Theme};
+use crate::xterm::{Terminal, TerminalOptions, Theme};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -35,64 +33,10 @@ pub fn embed_shell(
     terminal.load_addon(addon_webgl.clone().dyn_into::<WebglAddon>()?.into());
     addon_fit.fit();
 
-    let mut line = String::new();
-    let mut cursor_col = 0;
-
-    let term: Terminal = terminal.clone().dyn_into()?;
-
-    let callback = Closure::wrap(Box::new(move |e: OnKeyEvent| {
-        let event = e.dom_event();
-        match event.key_code() {
-            term_codes::KEY_ENTER => {
-                if !line.is_empty() {
-                    term.writeln("");
-                    term.writeln(&format!("You entered {} characters '{}'", line.len(), line));
-                    line.clear();
-                    cursor_col = 0;
-                }
-                term_printing::prompt(&term);
-            }
-            term_codes::KEY_BACKSPACE => {
-                if cursor_col > 0 {
-                    term.write("\u{0008} \u{0008}");
-                    line.pop();
-                    cursor_col -= 1;
-                }
-            }
-            term_codes::KEY_LEFT_ARROW => {
-                if cursor_col > 0 {
-                    term.write(term_codes::CURSOR_LEFT);
-                    cursor_col -= 1;
-                }
-            }
-            term_codes::KEY_RIGHT_ARROW => {
-                if cursor_col < line.len() {
-                    term.write(term_codes::CURSOR_RIGHT);
-                    cursor_col += 1;
-                }
-            }
-            term_codes::KEY_L if event.ctrl_key() => term.clear(),
-            term_codes::KEY_C if event.ctrl_key() => {
-                term_printing::prompt(&term);
-                line.clear();
-                cursor_col = 0;
-            }
-            _ => {
-                if !event.alt_key() && !event.alt_key() && !event.ctrl_key() && !event.meta_key() {
-                    term.write(&event.key());
-                    line.push_str(&e.key());
-                    cursor_col += 1;
-                }
-            }
-        }
-    }) as Box<dyn FnMut(_)>);
-
-    terminal.on_key(callback.as_ref().unchecked_ref());
-    callback.forget();
-
-    term_printing::prompt_nobreak(&terminal);
-    terminal.focus();
-
-    shell::Shell::global_mut(|ref mut s| s.attach(terminal));
+    shell::Shell::global_mut(|ref mut s| {
+        s.attach(terminal);
+        s.write_greeter();
+        s.focus();
+    });
     Ok(())
 }
