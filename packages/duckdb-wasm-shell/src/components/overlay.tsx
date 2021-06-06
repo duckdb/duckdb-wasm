08bde742935d589719472f4f6da209a6640dc7b6
Andre Kohn <kohn.a@outlook.com>
2021-06-06 18:16:59 +0200

    Test shell overlay

---
+++ packages/duckdb-wasm-shell/src/components/overlay.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './overlay.module.css';
+
+import { motion, AnimatePresence } from 'framer-motion';
+
+interface Props {
+    children?: React.ReactElement;
+}
+
+class Overlay extends React.Component<Props> {
+    public render(): React.ReactElement {
+        return (
+            <AnimatePresence>
+                <motion.div
+                    className={styles.overlay_container}
+                    initial={{ backgroundColor: 'rgba(0,0,0,0.2)' }}
+                    animate={{ backgroundColor: 'rgba(0,0,0,0.6)' }}
+                    transition={{ duration: 0.4 }}
+                >
+                    <motion.div
+                        className={styles.overlay}
+                        initial={{ translateY: 100 }}
+                        animate={{ translateY: 0 }}
+                        transition={{ duration: 0.4 }}
+                    >
+                        {this.props.children}
+                    </motion.div>
+                </motion.div>
+            </AnimatePresence>
+        );
+    }
+}
+
+export default Overlay;
