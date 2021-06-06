e2570fb46080f6a3a79ac533734391915b6d7af1
Andre Kohn <kohn.a@outlook.com>
2021-06-06 18:30:55 +0200

    Shell overlay styling and fonts

--- packages/duckdb-wasm-shell/src/shell.tsx
+++ packages/duckdb-wasm-shell/src/shell.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Overlay from './components/overlay';
+import FilesystemViewer from './components/filesystem_viewer';
 import * as shell from '../crate/pkg';
 import styles from './shell.module.css';
 import 'xterm/css/xterm.css';
@@ -47,7 +48,7 @@ class Shell extends React.Component<Props> {
         return (
             <div className={styles.root} style={style}>
                 <Overlay>
-                    <span>foo</span>
+                    <FilesystemViewer />
                 </Overlay>
                 <div ref={this.termContainer} className={styles.term_container}></div>
             </div>
