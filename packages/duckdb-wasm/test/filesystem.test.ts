8999e0caf43217983c60ed2c146d35a08f04b62c
Andre Kohn <kohn.a@outlook.com>
2021-06-18 17:20:39 +0200

    Add filesystem tests

--- packages/duckdb-wasm/test/filesystem.test.ts
+++ packages/duckdb-wasm/test/filesystem.test.ts
@@ -3,6 +3,12 @@ import * as arrow from 'apache-arrow';
 
 const decoder = new TextDecoder();
 
+function itBrowser(expectation: string, assertion?: jasmine.ImplementationCallback, timeout?: number): void {
+    if (typeof window !== 'undefined') {
+        it(expectation, assertion, timeout);
+    }
+}
+
 export function testFilesystem(
     db: () => duckdb.AsyncDuckDB,
     resolveData: (url: string) => Promise<Uint8Array | null>,
@@ -29,36 +35,34 @@ export function testFilesystem(
             );
         };
         it('File buffer used once', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
             await test();
         });
         it('File buffer used twice', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
             await test();
             await test();
         });
-        if (typeof window !== 'undefined') {
-            it('Blob used once', async () => {
-                const studenten = await resolveData('/uni/studenten.parquet');
-                expect(studenten).not.toBeNull();
-                const studenten_blob = new Blob([studenten!]);
-                const studenten_url = URL.createObjectURL(studenten_blob);
-                await db().registerFileURL('studenten.parquet', studenten_url);
-                await test();
-                URL.revokeObjectURL(studenten_url);
-            });
-        }
+        itBrowser('Blob used once', async () => {
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            const students_blob = new Blob([students!]);
+            const students_url = URL.createObjectURL(students_blob);
+            await db().registerFileURL('studenten.parquet', students_url);
+            await test();
+            URL.revokeObjectURL(students_url);
+        });
     });
 
     describe('Parquet Scans', () => {
-        it('single table', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
+        it('single table from buffer', async () => {
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
             const result = await conn.sendQuery(`SELECT matrnr FROM parquet_scan('studenten.parquet');`);
             const table = await arrow.Table.from<{ matrnr: arrow.Int }>(result);
             expect(table.getColumnAt(0)?.toArray()).toEqual(
@@ -66,21 +70,35 @@ export function testFilesystem(
             );
         });
 
+        itBrowser('single table from blob', async () => {
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            const students_blob = new Blob([students!]);
+            const students_url = URL.createObjectURL(students_blob);
+            await db().registerFileURL('studenten.parquet', students_url);
+            const result = await conn.sendQuery(`SELECT matrnr FROM parquet_scan('studenten.parquet');`);
+            const table = await arrow.Table.from<{ matrnr: arrow.Int }>(result);
+            expect(table.getColumnAt(0)?.toArray()).toEqual(
+                new Int32Array([24002, 25403, 26120, 26830, 27550, 28106, 29120, 29555]),
+            );
+            URL.revokeObjectURL(students_url);
+        });
+
         it('simple join', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
+            const students = await resolveData('/uni/studenten.parquet');
             const hoeren = await resolveData('/uni/hoeren.parquet');
             const vorlesungen = await resolveData('/uni/vorlesungen.parquet');
-            expect(studenten).not.toBeNull();
+            expect(students).not.toBeNull();
             expect(hoeren).not.toBeNull();
             expect(vorlesungen).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
+            await db().registerFileBuffer('studenten.parquet', students!);
             await db().registerFileBuffer('hoeren.parquet', hoeren!);
             await db().registerFileBuffer('vorlesungen.parquet', vorlesungen!);
 
             const result = await conn.sendQuery(`
-                    SELECT studenten.matrnr, vorlesungen.titel
-                    FROM parquet_scan('studenten.parquet') studenten
-                    INNER JOIN parquet_scan('hoeren.parquet') hoeren ON (studenten.matrnr = hoeren.matrnr)
+                    SELECT students.matrnr, vorlesungen.titel
+                    FROM parquet_scan('studenten.parquet') students
+                    INNER JOIN parquet_scan('hoeren.parquet') hoeren ON (students.matrnr = hoeren.matrnr)
                     INNER JOIN parquet_scan('vorlesungen.parquet') vorlesungen ON (vorlesungen.vorlnr = hoeren.vorlnr);
                 `);
             const table = await arrow.Table.from<{ matrnr: arrow.Int; titel: arrow.Utf8 }>(result);
@@ -110,14 +128,15 @@ export function testFilesystem(
     });
 
     describe('Writing', () => {
-        it('Copy To CSV', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
-            await db().registerEmptyFileBuffer('studenten.csv');
-            await conn.runQuery(`CREATE TABLE studenten AS SELECT * FROM parquet_scan('studenten.parquet');`);
-            await conn.runQuery(`COPY studenten TO 'studenten.csv' WITH (HEADER 1, DELIMITER ';', FORMAT CSV);`);
-            const outBuffer = await db().copyFileToBuffer('studenten.csv');
+        it('Copy To CSV Buffer', async () => {
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
+            await db().registerEmptyFileBuffer('students.csv');
+            await conn.runQuery(`CREATE TABLE students AS SELECT * FROM parquet_scan('studenten.parquet');`);
+            await conn.runQuery(`COPY students TO 'students.csv' WITH (HEADER 1, DELIMITER ';', FORMAT CSV);`);
+            await conn.runQuery(`DROP TABLE IF EXISTS students`);
+            const outBuffer = await db().copyFileToBuffer('students.csv');
             expect(outBuffer).not.toBeNull();
             const text = decoder.decode(outBuffer!);
             expect(text).toBe(`matrnr;name;semester
@@ -131,28 +150,56 @@ export function testFilesystem(
 29555;Feuerbach;2
 `);
         });
+
+        itBrowser('Copy To CSV Blob with COW', async () => {
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            const dummy = new Blob(['fooo']);
+            const dummy_url = URL.createObjectURL(dummy);
+            await db().registerFileBuffer('studenten.parquet', students!);
+            await db().registerFileURL('students.csv', dummy_url);
+            await conn.runQuery(`CREATE TABLE students AS SELECT * FROM parquet_scan('studenten.parquet');`);
+            await conn.runQuery(`COPY students TO 'students.csv' WITH (HEADER 1, DELIMITER ';', FORMAT CSV);`);
+            await conn.runQuery(`DROP TABLE IF EXISTS students`);
+            const outBuffer = await db().copyFileToBuffer('students.csv');
+            expect(outBuffer).not.toBeNull();
+            const text = decoder.decode(outBuffer!);
+            expect(text).toBe(`matrnr;name;semester
+24002;Xenokrates;18
+25403;Jonas;12
+26120;Fichte;10
+26830;Aristoxenos;8
+27550;Schopenhauer;6
+28106;Carnap;3
+29120;Theophrastos;2
+29555;Feuerbach;2
+`);
+            expect(await (await fetch(dummy_url)).text()).toBe('fooo');
+            URL.revokeObjectURL(dummy_url);
+        });
+
         it('Copy To Parquet', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
-            await db().registerEmptyFileBuffer('studenten2.parquet');
-            await conn.runQuery(`CREATE TABLE studenten2 AS SELECT * FROM parquet_scan('studenten.parquet');`);
-            await conn.runQuery(`COPY studenten2 TO 'studenten2.parquet' (FORMAT PARQUET);`);
-            const url = await db().copyFileToBuffer('studenten2.parquet');
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
+            await db().registerEmptyFileBuffer('students2.parquet');
+            await conn.runQuery(`CREATE TABLE students2 AS SELECT * FROM parquet_scan('studenten.parquet');`);
+            await conn.runQuery(`COPY students2 TO 'students2.parquet' (FORMAT PARQUET);`);
+            const url = await db().copyFileToBuffer('students2.parquet');
             expect(url).not.toBeNull();
         });
 
         it('Copy To Parquet And Load Again', async () => {
-            const studenten = await resolveData('/uni/studenten.parquet');
-            expect(studenten).not.toBeNull();
-            await db().registerFileBuffer('studenten.parquet', studenten!);
-            await db().registerEmptyFileBuffer('studenten3.parquet');
-            await conn.runQuery(`CREATE TABLE studenten3 AS SELECT * FROM parquet_scan('studenten.parquet');`);
-            await conn.runQuery(`COPY studenten3 TO 'studenten3.parquet' (FORMAT PARQUET);`);
-            const url = await db().copyFileToBuffer('studenten3.parquet');
+            const students = await resolveData('/uni/studenten.parquet');
+            expect(students).not.toBeNull();
+            await db().registerFileBuffer('studenten.parquet', students!);
+            await db().registerEmptyFileBuffer('students3.parquet');
+            await conn.runQuery(`CREATE TABLE students3 AS SELECT * FROM parquet_scan('studenten.parquet');`);
+            await conn.runQuery(`COPY students3 TO 'students3.parquet' (FORMAT PARQUET);`);
+            const url = await db().copyFileToBuffer('students3.parquet');
             expect(url).not.toBeNull();
-            await conn.runQuery(`CREATE TABLE studenten4 AS SELECT * FROM parquet_scan('studenten3.parquet');`);
-            const result = await conn.sendQuery(`SELECT matrnr FROM studenten4;`);
+            await conn.runQuery(`CREATE TABLE students4 AS SELECT * FROM parquet_scan('students3.parquet');`);
+            const result = await conn.sendQuery(`SELECT matrnr FROM students4;`);
             const table = await arrow.Table.from<{ matrnr: arrow.Int }>(result);
             expect(table.getColumnAt(0)?.toArray()).toEqual(
                 new Int32Array([24002, 25403, 26120, 26830, 27550, 28106, 29120, 29555]),
