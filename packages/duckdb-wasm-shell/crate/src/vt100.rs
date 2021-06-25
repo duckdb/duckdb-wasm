67107385dca971d45d4980f116f8f4076e18086f
Andre Kohn <kohn.a@outlook.com>
2021-06-25 11:17:20 +0200

    Add warning banner when running without native exception handling

--- packages/duckdb-wasm-shell/crate/src/vt100.rs
+++ packages/duckdb-wasm-shell/crate/src/vt100.rs
@@ -59,5 +59,7 @@ pub const CLEAR_SCREEN: &str = "\x1b[2J";
 pub const MODES_OFF: &str = "\x1b[m";
 pub const MODE_BOLD: &str = "\x1b[1m";
 pub const MODE_UNDERLINE: &str = "\x1b[4m";
+pub const MODE_BLINK: &str = "\x1b[6m";
+pub const MODE_REVERSE: &str = "\x1b[7m";
 
 pub const CURSOR_HOME: &str = "\x1b[H";
