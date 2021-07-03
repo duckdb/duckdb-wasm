e93f81d183f9f12d008562a31e1c350b9bfb6faa
Andre Kohn <kohn.a@outlook.com>
2021-07-03 13:52:31 +0200

    Upgrade emscripten to 2.0.25 and drop spdlog

--- lib/src/wasm_main.cc
+++ lib/src/wasm_main.cc
@@ -1,11 +1,2 @@
-#include "spdlog/sinks/stdout_sinks.h"
-#include "spdlog/spdlog.h"
-
 int main() {
-    // Prepare the logger
-    auto logSink = std::make_shared<spdlog::sinks::stderr_sink_st>();
-    auto logger = std::make_shared<spdlog::logger>("console", logSink);
-    logger->set_level(spdlog::level::debug);
-    logger->set_pattern(R"RAW({"time":"%T","level":"%l","message":"%v"})RAW");
-    spdlog::set_default_logger(logger);
 }
