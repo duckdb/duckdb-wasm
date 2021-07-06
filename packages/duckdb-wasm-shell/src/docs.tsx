d4b4eca7ca591c5ff2111acc5d3e9b322794089a
Andre Kohn <kohn.a@outlook.com>
2021-07-06 14:41:06 +0200

    Embed docs into shell as iframe

---
+++ packages/duckdb-wasm-shell/src/docs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as duckdb from '@duckdb/duckdb-wasm/dist/duckdb.module.js';
+import styles from './docs.module.css';
+
+export const Docs = (): React.ReactElement => (
+    <div
+        className={styles.root}
+        dangerouslySetInnerHTML={{
+            __html: `<iframe style="width:100%; height:100%" src='docs/index.html?version=${duckdb.PACKAGE_VERSION}' />`,
+        }}
+    />
+);
+
+export default Docs;
