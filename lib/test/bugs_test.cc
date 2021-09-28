3a6f6abc27bfca6c120500c06e6b2150a59548ee
Andre Kohn <kohn.a@outlook.com>
2021-09-28 20:06:42 +0200

    Add test with 0 scaling bug

---
+++ lib/test/bugs_test.cc
@@ -0,0 +1,29 @@
+#include <filesystem>
+#include <sstream>
+
+#include "duckdb/common/types/date.hpp"
+#include "duckdb/common/types/timestamp.hpp"
+#include "duckdb/execution/operator/persistent/buffered_csv_reader.hpp"
+#include "duckdb/web/io/ifstream.h"
+#include "duckdb/web/io/memory_filesystem.h"
+#include "duckdb/web/test/config.h"
+#include "duckdb/web/webdb.h"
+#include "gtest/gtest.h"
+#include "parquet-extension.hpp"
+
+using namespace duckdb::web;
+using namespace std;
+
+namespace {
+
+// https://github.com/duckdb/duckdb-wasm/issues/234
+TEST(Bugs, DISABLED_Decimals) {
+    auto db = make_shared<WebDB>();
+    WebDB::Connection conn{*db};
+    auto buffer = conn.RunQuery("SELECT 1::DECIMAL(2,1);");
+    ASSERT_TRUE(buffer.ok()) << buffer.status().message();
+    buffer = conn.RunQuery("SELECT 1::DECIMAL(2,0);");
+    ASSERT_TRUE(buffer.ok()) << buffer.status().message();
+}
+
+}  // namespace
