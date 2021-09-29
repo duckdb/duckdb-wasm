ed537140edf669a10fa43ecc2f93750eea9212fb
Andre Kohn <kohn.a@outlook.com>
2021-09-29 19:13:32 +0200

    Use ccache for building duckdb shell

--- scripts/build_duckdb_shell.sh
+++ scripts/build_duckdb_shell.sh
@@ -12,6 +12,12 @@ DUCKDB_BUILD_DIR="${DUCKDB_DIR}/build/Release"
 
 mkdir -p ${DUCKDB_BUILD_DIR}
 
-cmake -S ${DUCKDB_DIR} -B ${DUCKDB_BUILD_DIR} -DCMAKE_BUILD_TYPE=Release -DBUILD_TPCH_EXTENSION=1
+cmake \
+    -S ${DUCKDB_DIR} \
+    -B ${DUCKDB_BUILD_DIR} \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TPCH_EXTENSION=1
 
 cmake --build ${DUCKDB_BUILD_DIR} --parallel ${CORES}
