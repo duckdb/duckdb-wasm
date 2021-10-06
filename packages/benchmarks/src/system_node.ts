54552e3f1d0476d761ffbd626439cd9e468e4d30
Andre Kohn <kohn.a@outlook.com>
2021-10-06 10:27:47 +0200

    Work on benchmarks

--- packages/benchmarks/src/system_node.ts
+++ packages/benchmarks/src/system_node.ts
@@ -1,4 +1,4 @@
-//import * as duckdb from '@duckdb/duckdb-wasm/dist/duckdb.module';
+import * as duckdb from '@duckdb/duckdb-wasm/dist/duckdb.module';
 import * as duckdb_sync from '@duckdb/duckdb-wasm/dist/duckdb-node-sync';
 import {
     SystemBenchmarkContext,
@@ -7,29 +7,32 @@ import {
     AlasqlIntegerScanBenchmark,
     ArqueroIntegerScanBenchmark,
     LovefieldIntegerScanBenchmark,
+    DuckDBAsyncMaterializingIntegerScanBenchmark,
+    DuckDBAsyncStreamingIntegerScanBenchmark,
     DuckDBSyncMaterializingIntegerScanBenchmark,
+    DuckDBSyncStreamingIntegerScanBenchmark,
 } from './system';
 import { runSystemBenchmarks } from './suite';
 import initSQLJs from 'sql.js';
-//import Worker from 'web-worker';
+import Worker from 'web-worker';
 import path from 'path';
 
 async function main() {
     let duckdbDB: duckdb_sync.DuckDB | null = null;
-    // const duckdbAsyncDB: duckdb.AsyncDuckDB | null = null;
-    // const duckdbWorker: Worker | null = null;
+    let duckdbAsyncDB: duckdb.AsyncDuckDB | null = null;
+    let duckdbWorker: Worker | null = null;
 
-    // // Configure the worker
-    // const DUCKDB_BUNDLE = await duckdb.selectBundle({
-    //     asyncDefault: {
-    //         mainModule: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb.wasm'),
-    //         mainWorker: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-node-async.worker.js'),
-    //     },
-    //     asyncNext: {
-    //         mainModule: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-next.wasm'),
-    //         mainWorker: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-node-async-next.worker.js'),
-    //     },
-    // });
+    // Configure the worker
+    const DUCKDB_BUNDLE = await duckdb.selectBundle({
+        asyncDefault: {
+            mainModule: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb.wasm'),
+            mainWorker: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-node-async.worker.js'),
+        },
+        asyncNext: {
+            mainModule: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-next.wasm'),
+            mainWorker: path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb-node-async-next.worker.js'),
+        },
+    });
 
     // Setup sync DuckDB
     const logger = new duckdb_sync.VoidLogger();
@@ -39,10 +42,10 @@ async function main() {
         path.resolve(__dirname, '../../../duckdb-wasm/dist/duckdb.wasm'),
     ).instantiate();
 
-    // // Setup async DuckDB
-    // duckdbWorker = new Worker(DUCKDB_BUNDLE.mainWorker);
-    // duckdbAsyncDB = new duckdb.AsyncDuckDB(logger, duckdbWorker);
-    // await duckdbAsyncDB.instantiate(DUCKDB_BUNDLE.mainModule);
+    // Setup async DuckDB
+    duckdbWorker = new Worker(DUCKDB_BUNDLE.mainWorker);
+    duckdbAsyncDB = new duckdb.AsyncDuckDB(logger, duckdbWorker);
+    await duckdbAsyncDB.instantiate(DUCKDB_BUNDLE.mainModule);
 
     // Setup sql.js
     const sqljsConfig = await initSQLJs();
@@ -52,25 +55,41 @@ async function main() {
         seed: Math.random(),
     };
     const suite: SystemBenchmark[] = [
+        // Integer scans
+        new DuckDBAsyncMaterializingIntegerScanBenchmark(duckdbAsyncDB, 1000),
+        new DuckDBAsyncMaterializingIntegerScanBenchmark(duckdbAsyncDB, 10000),
+        new DuckDBAsyncMaterializingIntegerScanBenchmark(duckdbAsyncDB, 100000),
+        new DuckDBAsyncMaterializingIntegerScanBenchmark(duckdbAsyncDB, 1000000),
+        new DuckDBAsyncStreamingIntegerScanBenchmark(duckdbAsyncDB, 1000),
+        new DuckDBAsyncStreamingIntegerScanBenchmark(duckdbAsyncDB, 10000),
+        new DuckDBAsyncStreamingIntegerScanBenchmark(duckdbAsyncDB, 100000),
+        new DuckDBAsyncStreamingIntegerScanBenchmark(duckdbAsyncDB, 1000000),
         new DuckDBSyncMaterializingIntegerScanBenchmark(duckdbDB, 1000),
         new DuckDBSyncMaterializingIntegerScanBenchmark(duckdbDB, 10000),
         new DuckDBSyncMaterializingIntegerScanBenchmark(duckdbDB, 100000),
         new DuckDBSyncMaterializingIntegerScanBenchmark(duckdbDB, 1000000),
-        new LovefieldIntegerScanBenchmark(1000),
-        new AlasqlIntegerScanBenchmark(1000),
-        new AlasqlIntegerScanBenchmark(10000),
-        new AlasqlIntegerScanBenchmark(100000),
-        new ArqueroIntegerScanBenchmark(1000),
-        new ArqueroIntegerScanBenchmark(10000),
-        new ArqueroIntegerScanBenchmark(100000),
-        new ArqueroIntegerScanBenchmark(1000000),
+        new DuckDBSyncStreamingIntegerScanBenchmark(duckdbDB, 1000),
+        new DuckDBSyncStreamingIntegerScanBenchmark(duckdbDB, 10000),
+        new DuckDBSyncStreamingIntegerScanBenchmark(duckdbDB, 100000),
+        new DuckDBSyncStreamingIntegerScanBenchmark(duckdbDB, 1000000),
         new SqljsIntegerScanBenchmark(sqljsDB, 1000),
         new SqljsIntegerScanBenchmark(sqljsDB, 10000),
         new SqljsIntegerScanBenchmark(sqljsDB, 100000),
         new SqljsIntegerScanBenchmark(sqljsDB, 1000000),
+        new ArqueroIntegerScanBenchmark(1000),
+        new ArqueroIntegerScanBenchmark(10000),
+        new ArqueroIntegerScanBenchmark(100000),
+        new ArqueroIntegerScanBenchmark(1000000),
+        new AlasqlIntegerScanBenchmark(1000),
+        new AlasqlIntegerScanBenchmark(10000),
+        new AlasqlIntegerScanBenchmark(100000),
+        new LovefieldIntegerScanBenchmark(1000),
     ];
     const results = await runSystemBenchmarks(ctx, suite);
     console.log(results);
+
+    // Terminate the worker
+    duckdbAsyncDB.terminate();
 }
 
 main();
