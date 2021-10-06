1b643c585edd6fd65cba01f040898831122b92cf
Andre Kohn <kohn.a@outlook.com>
2021-10-06 19:48:43 +0200

    Add generator for groups

--- packages/benchmarks/src/system/data_generator.ts
+++ packages/benchmarks/src/system/data_generator.ts
@@ -61,3 +61,32 @@ export function generateArrowUtf8Table(n: number, chars: number): [arrow.Schema,
     }
     return [schema, batches];
 }
+
+export function generateArrowGroupedInt32Table(n: number, group: number): [arrow.Schema, arrow.RecordBatch[]] {
+    const values0 = [];
+    const values1 = [];
+    for (let i = 0; i < n; ++i) {
+        values0.push(i);
+    }
+    for (let i = 0; i < n; ++i) {
+        values1.push(Math.floor(i % group));
+    }
+    shuffle(values0);
+    shuffle(values1);
+    const schema = new arrow.Schema([
+        new arrow.Field('v0', new arrow.Int32()),
+        new arrow.Field('v1', new arrow.Int32()),
+    ]);
+    const batches = [];
+    for (let i = 0; i < n; ) {
+        const rows = Math.min(1000, n - i);
+        batches.push(
+            new arrow.RecordBatch(schema, rows, [
+                arrow.Int32Vector.from(values0.slice(i, i + n)),
+                arrow.Int32Vector.from(values1.slice(i, i + n)),
+            ]),
+        );
+        i += rows;
+    }
+    return [schema, batches];
+}
