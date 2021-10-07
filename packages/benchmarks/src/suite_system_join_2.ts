8803d3e7eb6def6e903ad03480b4c1cbe9a9c1d8
Andre Kohn <kohn.a@outlook.com>
2021-10-07 14:01:49 +0200

    Working on benchmarks

--- packages/benchmarks/src/suite_system_join_2.ts
+++ packages/benchmarks/src/suite_system_join_2.ts
@@ -13,14 +13,14 @@ async function main() {
         seed: Math.random(),
     };
     const suite: SystemBenchmark[] = [
-        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 1000, 10000, 100, 10),
-        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 10000, 100000, 100, 10),
-        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 100000, 100000, 100, 10),
-        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 100000, 1000000, 100, 10),
         new ArqueroIntegerJoin2Benchmark(1000, 10000, 100, 10),
         new ArqueroIntegerJoin2Benchmark(10000, 100000, 100, 10),
         new ArqueroIntegerJoin2Benchmark(100000, 100000, 100, 10),
         new ArqueroIntegerJoin2Benchmark(100000, 1000000, 100, 10),
+        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 1000, 10000, 100, 10),
+        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 10000, 100000, 100, 10),
+        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 100000, 100000, 100, 10),
+        new DuckDBSyncIntegerJoin2Benchmark(duckdbDB, 100000, 1000000, 100, 10),
     ];
     const results = await runSystemBenchmarks(ctx, suite);
     await writeReport(results, './benchmark_system_join_2.json');
--- packages/benchmarks/src/suite_system_regex.ts
+++ packages/benchmarks/src/suite_system_regex.ts
@@ -8,12 +8,12 @@ async function main() {
         seed: Math.random(),
     };
     const suite: SystemBenchmark[] = [
-        new DuckDBSyncRegexBenchmark(duckdbDB, 1000, 20),
-        new DuckDBSyncRegexBenchmark(duckdbDB, 10000, 20),
-        new DuckDBSyncRegexBenchmark(duckdbDB, 100000, 20),
         new ArqueroRegexBenchmark(1000, 20),
         new ArqueroRegexBenchmark(10000, 20),
         new ArqueroRegexBenchmark(100000, 20),
+        new DuckDBSyncRegexBenchmark(duckdbDB, 1000, 20),
+        new DuckDBSyncRegexBenchmark(duckdbDB, 10000, 20),
+        new DuckDBSyncRegexBenchmark(duckdbDB, 100000, 20),
     ];
     const results = await runSystemBenchmarks(ctx, suite);
     console.log(results);
