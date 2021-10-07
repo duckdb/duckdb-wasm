bb412b04d2786e0f4b1e1ce5d5325ee574fc43b4
Andre Kohn <kohn.a@outlook.com>
2021-10-07 16:59:22 +0200

    Pack tpch as parquet and arrow files

--- scripts/generate_tpch_tbl.sh
+++ scripts/generate_tpch_tbl.sh
@@ -7,13 +7,11 @@ trap exit SIGINT
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 TPCH_DBGEN_DIR="${PROJECT_ROOT}/submodules/tpch-dbgen/dbgen"
 TPCH_DBGEN=${TPCH_DBGEN_DIR}/dbgen
-DATAPREP=${PROJECT_ROOT}/target/release/dataprep
 SCALE_FACTOR=${1:-0.01}
 SCALE_FACTOR_DIR=${SCALE_FACTOR/./_}
 TPCH_DIR=${PROJECT_ROOT}/data/tpch
 TPCH_SF_OUT=${TPCH_DIR}/${SCALE_FACTOR_DIR}
 TPCH_SF_OUT_TBL=${TPCH_SF_OUT}/tbl
-TPCH_SF_OUT_DATA=${TPCH_SF_OUT}/parquet
 
 echo "SCALE_FACTOR=${SCALE_FACTOR}"
 
@@ -31,12 +29,6 @@ fi
 chmod +x ${TPCH_DBGEN}
 echo "TPCH_DBGEN=${TPCH_DBGEN}"
 
-if [ ! -f ${DATAPREP} ]; then
-    cargo build --manifest-path=${PROJECT_ROOT}/Cargo.toml --release -p dataprep
-fi
-chmod +x ${DATAPREP}
-echo "DATAPREP=${DATAPREP}"
-
 mkdir -p ${TPCH_SF_OUT_TBL}
 TBL_COUNT=$(find ${TPCH_SF_OUT_TBL} -name "*.tbl" | wc -l)
 if [[ ${TBL_COUNT} -ne 8 ]]; then
@@ -44,11 +36,3 @@ if [[ ${TBL_COUNT} -ne 8 ]]; then
     DSS_PATH=${TPCH_SF_OUT_TBL} ./dbgen -vf -s ${SCALE_FACTOR}
 fi
 echo "TPCH_SF_OUT_TBL=${TPCH_SF_OUT_TBL}"
-
-mkdir -p ${TPCH_SF_OUT_DATA}
-DATA_COUNT=$(find ${TPCH_SF_OUT_TBL} -name "*.parquet" | wc -l)
-if [[ ${DATA_COUNT} -ne 8 ]]; then
-    rm -rf ${TPCH_SF_OUT_DATA}/*.parquet
-    ${DATAPREP} tpch -i ${TPCH_SF_OUT_TBL} -o ${TPCH_SF_OUT_DATA}
-fi
-echo "TPCH_SF_OUT_DATA=${TPCH_SF_OUT_DATA}"
