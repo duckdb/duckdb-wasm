596c117f0d5dd682ca21dd2d3230b093b25514fe
Andre Kohn <kohn.a@outlook.com>
2021-10-07 14:00:44 +0200

    Use setup helpers for internal benchmarks

--- packages/benchmarks/src/suite_internal.ts
+++ packages/benchmarks/src/suite_internal.ts
@@ -1,44 +1,16 @@
-import * as duckdb from '@duckdb/duckdb-wasm/dist/duckdb-esm';
-import * as duckdb_sync from '@duckdb/duckdb-wasm/dist/duckdb-node-sync';
-import path from 'path';
-import Worker from 'web-worker';
-
 import { benchmarkFormat, benchmarkIterator, benchmarkIteratorAsync } from './internal';
 import { runBenchmarks } from './suite';
 import Benchmark from 'buffalo-bench/lib';
+import { setupDuckDBAsync, setupDuckDBSync } from './setup';
 
 async function main() {
-    let db: duckdb_sync.DuckDB | null = null;
-    let adb: duckdb.AsyncDuckDB | null = null;
-    let worker: Worker | null = null;
-
-    // Configure the worker
-    const DUCKDB_BUNDLE = await duckdb.selectBundle({
-        asyncDefault: {
-            mainModule: path.resolve(__dirname, '../../duckdb-wasm/dist/duckdb.wasm'),
-            mainWorker: path.resolve(__dirname, '../../duckdb-wasm/dist/duckdb-node-async.worker.js'),
-        },
-        asyncNext: {
-            mainModule: path.resolve(__dirname, '../../duckdb-wasm/dist/duckdb-next.wasm'),
-            mainWorker: path.resolve(__dirname, '../../duckdb-wasm/dist/duckdb-node-async-next.worker.js'),
-        },
-    });
-
-    const logger = new duckdb_sync.VoidLogger();
-    db = await new duckdb_sync.DuckDB(
-        logger,
-        duckdb_sync.NODE_RUNTIME,
-        path.resolve(__dirname, '../../duckdb-wasm/dist/duckdb.wasm'),
-    ).instantiate();
-
-    worker = new Worker(DUCKDB_BUNDLE.mainWorker);
-    adb = new duckdb.AsyncDuckDB(logger, worker);
-    await adb.instantiate(DUCKDB_BUNDLE.mainModule);
+    const duckdbSync = await setupDuckDBSync();
+    const duckdbAsync = await setupDuckDBAsync();
 
     let suite: Benchmark[] = [];
-    suite = suite.concat(benchmarkFormat(() => db!));
-    suite = suite.concat(benchmarkIterator(() => db!));
-    suite = suite.concat(benchmarkIteratorAsync(() => adb!));
+    suite = suite.concat(benchmarkFormat(() => duckdbSync!));
+    suite = suite.concat(benchmarkIterator(() => duckdbSync!));
+    suite = suite.concat(benchmarkIteratorAsync(() => duckdbAsync!));
     await runBenchmarks(suite);
 }
 
