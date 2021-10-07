f27cb4c2305f67404364434276d3787789f313f5
Andre Kohn <kohn.a@outlook.com>
2021-10-07 13:45:08 +0200

    Add benchmarks

---
+++ packages/benchmarks/src/suite_system_sort_int.ts
@@ -0,0 +1,42 @@
+import { setupDuckDBSync, writeReport } from './setup';
+import {
+    SystemBenchmarkContext,
+    SystemBenchmark,
+    ArqueroIntegerSortBenchmark,
+    DuckDBSyncIntegerSortBenchmark,
+    DuckDBSyncIntegerTopKBenchmark,
+    ArqueroIntegerTopKBenchmark,
+} from './system';
+import { runSystemBenchmarks } from './suite';
+
+async function main() {
+    const duckdbSync = await setupDuckDBSync();
+    const ctx: SystemBenchmarkContext = {
+        seed: Math.random(),
+    };
+    const suite: SystemBenchmark[] = [
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 1000, 1, 1),
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 10000, 1, 1),
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 100000, 1, 1),
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 1000, 2, 2),
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 10000, 2, 2),
+        new DuckDBSyncIntegerSortBenchmark(duckdbSync, 100000, 2, 2),
+        new DuckDBSyncIntegerTopKBenchmark(duckdbSync, 1000, 1, 1, 100),
+        new DuckDBSyncIntegerTopKBenchmark(duckdbSync, 10000, 1, 1, 100),
+        new DuckDBSyncIntegerTopKBenchmark(duckdbSync, 100000, 1, 1, 100),
+        new ArqueroIntegerSortBenchmark(1000, 1, 1),
+        new ArqueroIntegerSortBenchmark(10000, 1, 1),
+        new ArqueroIntegerSortBenchmark(100000, 1, 1),
+        new ArqueroIntegerSortBenchmark(1000, 2, 2),
+        new ArqueroIntegerSortBenchmark(10000, 2, 2),
+        new ArqueroIntegerSortBenchmark(100000, 2, 2),
+        new ArqueroIntegerTopKBenchmark(1000, 1, 1, 100),
+        new ArqueroIntegerTopKBenchmark(10000, 1, 1, 100),
+        new ArqueroIntegerTopKBenchmark(100000, 1, 1, 100),
+    ];
+    const results = await runSystemBenchmarks(ctx, suite);
+    console.log(results);
+    await writeReport(results, './benchmark_system_sort_int.json');
+}
+
+main();
