44f4db4142df053cdeff2184b6018e1cc22ef96a
Andre Kohn <kohn.a@outlook.com>
2021-10-11 01:09:34 +0200

    Load benchmarks files

--- packages/duckdb-wasm-shell/src/benchmarks.tsx
+++ packages/duckdb-wasm-shell/src/benchmarks.tsx
@@ -1,9 +1,45 @@
+import * as arrow from 'apache-arrow';
 import React from 'react';
-
 import styles from './benchmarks.module.css';
+
+const DATA_URL = 'https://shell.duckdb.org/data/benchmarks.arrow';
 
 type Props = Record<string, string>;
 
+enum LoadingStatus {
+    PENDING,
+    INFLIGHT,
+    FAILED,
+    SUCCEEDED,
+}
+
+interface State {
+    status: LoadingStatus;
+    table: arrow.Table | null;
+}
+
 export const Benchmarks: React.FC<Props> = (props: Props) => {
+    const [state, setState] = React.useState<State>({
+        status: LoadingStatus.PENDING,
+        table: null,
+    });
+
+    const fetch_data = async () => {
+        const data = await fetch(DATA_URL);
+        const buffer = await data.arrayBuffer();
+        setState(s => ({
+            ...s,
+            status: LoadingStatus.SUCCEEDED,
+            table: arrow.Table.from(new Uint8Array(buffer)),
+        }));
+    };
+
+    if (state.status == LoadingStatus.PENDING && state.table == null) {
+        setState(s => ({
+            ...s,
+            status: LoadingStatus.INFLIGHT,
+        }));
+        fetch_data();
+    }
     return <div className={styles.root}>foo</div>;
 };
