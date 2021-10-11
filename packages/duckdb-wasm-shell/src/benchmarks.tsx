e96ad13e8b32f11c305046126de84a47fdc7aacd
Andre Kohn <kohn.a@outlook.com>
2021-10-11 10:14:34 +0200

    Read benchmark entries

--- packages/duckdb-wasm-shell/src/benchmarks.tsx
+++ packages/duckdb-wasm-shell/src/benchmarks.tsx
@@ -1,10 +1,9 @@
 import * as arrow from 'apache-arrow';
 import React from 'react';
 import styles from './benchmarks.module.css';
+import { BenchmarkType, BenchmarkEntry, readBenchmarks, groupBenchmarks } from './benchmark_reader';
 
 const DATA_URL = 'https://shell.duckdb.org/data/benchmarks.arrow';
-
-type Props = Record<string, string>;
 
 enum LoadingStatus {
     PENDING,
@@ -13,25 +12,39 @@ enum LoadingStatus {
     SUCCEEDED,
 }
 
+type Props = Record<string, string>;
+
 interface State {
     status: LoadingStatus;
-    table: arrow.Table | null;
+    table: arrow.Table<BenchmarkType> | null;
+    benchmarks: BenchmarkEntry[];
+    benchmarkGroups: Map<string, BenchmarkEntry[]>;
 }
 
 export const Benchmarks: React.FC<Props> = (props: Props) => {
     const [state, setState] = React.useState<State>({
         status: LoadingStatus.PENDING,
         table: null,
+        benchmarks: [],
+        benchmarkGroups: new Map(),
     });
 
     const fetch_data = async () => {
         const data = await fetch(DATA_URL);
         const buffer = await data.arrayBuffer();
-        setState(s => ({
-            ...s,
-            status: LoadingStatus.SUCCEEDED,
-            table: arrow.Table.from(new Uint8Array(buffer)),
-        }));
+        setState(s => {
+            const table = arrow.Table.from(new Uint8Array(buffer));
+            const entries = readBenchmarks(table);
+            const entryGroups = groupBenchmarks(entries);
+            console.log(entryGroups);
+            return {
+                ...s,
+                status: LoadingStatus.SUCCEEDED,
+                table: arrow.Table.from(new Uint8Array(buffer)),
+                benchmarks: entries,
+                benchmarkGroups: entryGroups,
+            };
+        });
     };
 
     if (state.status == LoadingStatus.PENDING && state.table == null) {
