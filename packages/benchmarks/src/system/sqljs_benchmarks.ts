3fdec01e4069aabf09660911014db103cb3c92bc
Andre Kohn <kohn.a@outlook.com>
2021-10-11 17:38:28 +0200

    Add sqljs integer sum benchmark

--- packages/benchmarks/src/system/sqljs_benchmarks.ts
+++ packages/benchmarks/src/system/sqljs_benchmarks.ts
@@ -4,7 +4,7 @@ import * as sqljs from 'sql.js';
 import { sqlCreate, sqlInsert } from './simple_sql';
 import { SystemBenchmark, SystemBenchmarkMetadata, SystemBenchmarkContext, noop } from './system_benchmark';
 import { getTPCHQuery, getTPCHSQLiteDB } from './tpch_loader';
-import { generateInt32, generateUtf8 } from './data_generator';
+import { generateGroupedInt32, generateInt32, generateUtf8 } from './data_generator';
 
 export class SqljsTPCHBenchmark implements SystemBenchmark {
     initDB: sqljs.SqlJsStatic;
@@ -212,6 +212,65 @@ export class SqljsRegexBenchmark implements SystemBenchmark {
         }
         if (n !== 10) {
             throw Error(`invalid tuple count. expected 10, received ${n}`);
+        }
+    }
+    async afterEach(_ctx: SystemBenchmarkContext): Promise<void> {}
+    async afterAll(_ctx: SystemBenchmarkContext): Promise<void> {
+        this.database!.run(`DROP TABLE IF EXISTS ${this.getName()}`);
+    }
+    async onError(_ctx: SystemBenchmarkContext): Promise<void> {
+        this.database!.run(`DROP TABLE IF EXISTS ${this.getName()}`);
+    }
+}
+
+export class SqljsIntegerSumBenchmark implements SystemBenchmark {
+    initDB: sqljs.SqlJsStatic;
+    database: sqljs.Database | null;
+    tuples: number;
+    groupSize: number;
+
+    constructor(initDB: sqljs.SqlJsStatic, tuples: number, groupSize: number) {
+        this.initDB = initDB;
+        this.database = null;
+        this.tuples = tuples;
+        this.groupSize = groupSize;
+    }
+    getName(): string {
+        return `sqljs_integer_sum_${this.tuples}`;
+    }
+    getMetadata(): SystemBenchmarkMetadata {
+        return {
+            benchmark: 'integer_sum',
+            system: 'sqljs',
+            tags: [],
+            timestamp: +new Date(),
+            parameters: [this.tuples, this.groupSize],
+        };
+    }
+    async beforeAll(ctx: SystemBenchmarkContext): Promise<void> {
+        faker.seed(ctx.seed);
+        const [values0, values1] = generateGroupedInt32(this.tuples, this.groupSize);
+        this.database = new this.initDB.Database();
+        const schema = new arrow.Schema([
+            new arrow.Field('v0', new arrow.Int32()),
+            new arrow.Field('v1', new arrow.Int32()),
+        ]);
+        this.database.run(sqlCreate(this.getName(), schema.fields));
+        for (const query of sqlInsert(this.getName(), schema.fields, [values0, values1])) {
+            this.database.run(query);
+        }
+    }
+    async beforeEach(_ctx: SystemBenchmarkContext): Promise<void> {}
+    async run(_ctx: SystemBenchmarkContext): Promise<void> {
+        const results = this.database!.exec(`SELECT SUM(v1) FROM ${this.getName()} GROUP BY v0`);
+        let n = 0;
+        for (const row of results[0].values) {
+            noop(row);
+            n += 1;
+        }
+        const expectedGroups = this.tuples / this.groupSize;
+        if (n !== expectedGroups) {
+            throw Error(`invalid tuple count. expected ${expectedGroups}, received ${n}`);
         }
     }
     async afterEach(_ctx: SystemBenchmarkContext): Promise<void> {}
