da04717fc99972357419cb187519723ea43dbe6e
Andre Kohn <kohn.a@outlook.com>
2021-10-13 22:10:21 +0200

    Add lovefield queries

--- packages/benchmarks/src/suite_system_tpch_lovefield.ts
+++ packages/benchmarks/src/suite_system_tpch_lovefield.ts
@@ -14,8 +14,10 @@ async function main() {
 
     const baseDir = path.resolve(__dirname, '../../../');
     const benchmarks: SystemBenchmark[] = [];
-    //for (const q of [1, 3, 4, 5, 6, 7, 8, 10, 12]) {
-    for (const q of [12]) {
+    for (const q of [1, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14]) {
+        if (sf >= 0.25 && q == 9) {
+            continue;
+        }
         benchmarks.push(new LovefieldTPCHBenchmark(sf, q));
     }
     const ctx: SystemBenchmarkContext = {
