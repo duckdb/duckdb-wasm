be2f9972e2e2f756b0554c53321bb139c85afe98
Andre Kohn <kohn.a@outlook.com>
2021-10-13 10:11:01 +0200

    Add lovefield benchmarks

--- packages/duckdb-wasm-shell/src/benchmark_table.tsx
+++ packages/duckdb-wasm-shell/src/benchmark_table.tsx
@@ -3,7 +3,7 @@ import { GroupedBenchmarks } from './benchmark_reader';
 
 import styles from './benchmark_table.module.css';
 
-const SYSTEMS = ['duckdb_sync', 'sqljs', 'lovefield', 'arquero'];
+const SYSTEMS = ['duckdb_sync', 'sqljs', 'arquero', 'lovefield'];
 
 enum Metric {
     MEAN_TIME,
@@ -20,8 +20,8 @@ const BenchmarkGroupHeader: React.FC<GroupHeaderProps> = (props: GroupHeaderProp
         <div className={styles.row_group_anchor} />
         <div className={styles.row_group_column_header}>DuckDB-wasm</div>
         <div className={styles.row_group_column_header}>sql.js</div>
-        <div className={styles.row_group_column_header}>Lovefield</div>
         <div className={styles.row_group_column_header}>Arquero</div>
+        <div className={styles.row_group_column_header}>Lovefield</div>
     </>
 );
 
