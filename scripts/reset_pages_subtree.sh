00a34cc5b9bd2045f32761169b9929bfec3f5e83
Andre Kohn <kohn.a@outlook.com>
2021-10-18 16:31:00 +0200

    Add script to reset pages worktree

---
+++ scripts/reset_pages_subtree.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
+PAGES_DIR=${PROJECT_ROOT}/worktrees/gh-pages
+
+mkdir -p ${PROJECT_ROOT}/worktrees
+if [ ! -d ${PAGES_DIR} ]; then
+    echo "[ RUN ] Add worktree origin/gh-pages"
+    git worktree add ${PAGES_DIR} origin/gh-pages
+fi
+
+DEFAULT_BRANCH="master"
+CURRENT_BRANCH=${1:-master}
+
+cd ${PAGES_DIR}
+git fetch origin gh-pages
+git reset --hard origin/gh-pages
