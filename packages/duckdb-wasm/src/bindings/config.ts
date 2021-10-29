adbbd3b016dadaf419ffd2a8d4f461a1555a34f7
Dominik Moritz <domoritz@gmail.com>
2021-10-29 11:47:34 -0400

    style: use js comment style

--- packages/duckdb-wasm/src/bindings/config.ts
+++ packages/duckdb-wasm/src/bindings/config.ts
@@ -1,10 +1,16 @@
 /** A DuckDB Config */
 export interface DuckDBConfig {
-    /// The database path
+    /**
+     * The database path
+     */
     path?: string;
-    /// Emit BigInts?
+    /**
+     * Emit BigInts?
+     */
     emitBigInt?: boolean;
-    /// The maximum number of threads.
-    /// Note that this will only work with cross-origin isolated sites since it requires SharedArrayBuffers.
+    /**
+     * The maximum number of threads.
+     * Note that this will only work with cross-origin isolated sites since it requires SharedArrayBuffers.
+     */
     maximumThreads?: number;
 }
