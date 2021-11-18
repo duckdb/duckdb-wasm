e7dcf04e2d882e2bc8553d1ad915965b418ef32e
Ian Johnson <ian@observablehq.com>
2021-11-18 12:32:23 -0500

    add failing test for #393

---
+++ packages/duckdb-wasm/test/regression/github_393.test.ts
@@ -0,0 +1,33 @@
+import * as duckdb from '../../src';
+import * as arrow from 'apache-arrow';
+
+// https://github.com/duckdb/duckdb-wasm/issues/332
+export function testGitHubIssue393(db: () => duckdb.AsyncDuckDB): void {
+    let conn: duckdb.AsyncDuckDBConnection;
+    beforeEach(async () => {
+        await db().flushFiles();
+        conn = await db().connect();
+    });
+    afterEach(async () => {
+        await conn.close();
+        await db().flushFiles();
+        await db().dropFiles();
+    });
+    describe('GitHub issues', () => {
+        it('332', async () => {
+            await db().registerFileText(
+                'test_date.csv',
+                `iso_str,time_in_ms
+2021-11-18 16:49:02,1637254142000
+`,
+            );
+            await conn.query("CREATE TABLE test_date AS SELECT * FROM 'test_date.csv'");
+            const date = await conn.query<{
+                iso_str: arrow.DateMillisecond;
+            }>('SELECT iso_str FROM test_date');
+            // expect to receive a JavaScript Date object when we get the value
+            expect(typeof date.toArray()[0].getMonth()).toEqual("function");
+            await conn.query('DROP TABLE test_date');
+        });
+    });
+}
