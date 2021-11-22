84ead1066ee716731e30420dcae81f72e1fc7e7b
Andre Kohn <kohn.a@outlook.com>
2021-11-22 12:30:07 +0100

    chore: add logo and badge to npm package

--- scripts/npm_version.sh
+++ scripts/npm_version.sh
@@ -30,6 +30,9 @@ node ${PROJECT_ROOT}/scripts/sync_versions.mjs
 
 if [[ "$1" = "publish" ]] ; then
 	cd ${PROJECT_ROOT}/packages/duckdb-wasm
+	mkdir -p ./dist/img
+	cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
+	${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
 
 	npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
 	npm publish --ignore-scripts --access public ${TAG} || true
