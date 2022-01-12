44b3e5e9cae7aa8292d620f0f2319463ccc24825
Andre Kohn <kohn.a@outlook.com>
2022-01-12 09:27:30 +0100

    ci: change deployment commit message

--- scripts/deploy_pages.sh
+++ scripts/deploy_pages.sh
@@ -56,5 +56,5 @@ fi
 git config user.name 'github-actions[bot]'
 git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git add -A .
-git commit --amend --reset-author -m "Deploy GitHub Page"
+git commit --amend --reset-author -m "Deploy GitHub Pages"
 git push origin HEAD:gh-pages --force
