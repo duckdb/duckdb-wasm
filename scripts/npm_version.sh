4042e5e573d6a7570208be61a654430eb3e0e9e7
Andre Kohn <kohn.a@outlook.com>
2022-01-13 11:57:38 +0100

    ci: publish standalone shell library on npm

--- scripts/npm_version.sh
+++ scripts/npm_version.sh
@@ -36,4 +36,12 @@ if [[ "$1" = "publish" ]] ; then
 
 	npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
 	npm publish --ignore-scripts --access public ${TAG} || true
+
+	cd ${PROJECT_ROOT}/packages/duckdb-wasm-shell
+	mkdir -p ./dist/img
+	cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
+	${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
+
+	npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
+	npm publish --ignore-scripts --access public ${TAG} || true
 fi
