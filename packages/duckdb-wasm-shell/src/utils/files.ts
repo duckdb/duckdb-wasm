ead2fa8a55821507da408e5fa38bc4167a2d42d3
Andre Kohn <kohn.a@outlook.com>
2022-01-13 10:53:00 +0100

    fix(shell): correctly resume the shell if the user canceled file picking

--- packages/duckdb-wasm-shell/src/utils/files.ts
+++ packages/duckdb-wasm-shell/src/utils/files.ts
@@ -4,7 +4,10 @@ export function pickFiles(db: duckdb.AsyncDuckDB): Promise<number> {
     return new Promise<number>((resolve, _reject) => {
         const input = document.createElement('input');
         input.type = 'file';
-        input.onchange = async function () {
+        let closed = false;
+        const finish = async () => {
+            console.log(input.files);
+            closed = true;
             const files = input.files as FileList;
             for (let i = 0; i < files.length; ++i) {
                 const file = files.item(i)!;
@@ -13,9 +16,22 @@ export function pickFiles(db: duckdb.AsyncDuckDB): Promise<number> {
             }
             resolve(files.length);
         };
-        input.onabort = function () {
-            console.log('abort');
-            resolve(0);
+        const deferredFinish = async () => {
+            window.removeEventListener('focus', deferredFinish);
+            if (closed) {
+                return;
+            }
+            await new Promise(r => setTimeout(r, 1000));
+            if (!closed) {
+                await finish();
+            }
+        };
+        input.onchange = async function () {
+            window.removeEventListener('focus', deferredFinish);
+            await finish();
+        };
+        input.onclick = async function () {
+            window.addEventListener('focus', deferredFinish);
         };
         input.click();
     });
