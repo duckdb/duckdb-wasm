0d2eb4e3109d1203ca147dd51c3571dd68604de8
Andre Kohn <kohn.a@outlook.com>
2022-01-15 18:26:39 +0100

    parse udf definition members

--- lib/src/udf.cc
+++ lib/src/udf.cc
@@ -24,12 +24,12 @@ namespace {
 enum FieldTag {
     NAME,
     RETURN_TYPE,
-    ARGUMENT_TYPE,
+    ARGUMENT_TYPES,
     CODE,
 };
 
 static std::unordered_map<std::string_view, FieldTag> FIELD_TAGS{{"name", FieldTag::NAME},
-                                                                 {"argumentTypes", FieldTag::ARGUMENT_TYPE},
+                                                                 {"argumentTypes", FieldTag::ARGUMENT_TYPES},
                                                                  {"returnType", FieldTag::RETURN_TYPE},
                                                                  {"code", FieldTag::CODE}};
 
@@ -54,7 +54,24 @@ arrow::Status UDFFunctionDefinition::ReadFrom(const rapidjson::Document& doc) {
                 ARROW_RETURN_NOT_OK(RequireFieldType(iter->value, rapidjson::Type::kStringType, name));
                 code = {iter->value.GetString(), iter->value.GetStringLength()};
                 break;
-                // XXX Parse more
+
+            case FieldTag::RETURN_TYPE: {
+                ARROW_RETURN_NOT_OK(RequireFieldType(iter->value, rapidjson::Type::kObjectType, name));
+                const auto& type_obj = iter->value.GetObject();
+                ARROW_ASSIGN_OR_RAISE(return_type, json::ReadType(type_obj));
+                break;
+            }
+
+            case FieldTag::ARGUMENT_TYPES:
+                ARROW_RETURN_NOT_OK(RequireFieldType(iter->value, rapidjson::Type::kArrayType, name));
+                const auto& args = iter->value.GetArray();
+                for (auto arg_iter = args.begin(); arg_iter != args.end(); ++arg_iter) {
+                    ARROW_RETURN_NOT_OK(RequireFieldType(*arg_iter, rapidjson::Type::kObjectType, name));
+                    const auto& arg_obj = arg_iter->GetObject();
+                    ARROW_ASSIGN_OR_RAISE(auto arg_type, json::ReadType(arg_obj));
+                    argument_types.push_back(arg_type);
+                }
+                break;
         }
     }
     return arrow::Status::OK();
