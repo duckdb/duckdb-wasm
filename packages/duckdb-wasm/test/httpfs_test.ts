687e6173b6212064bf75de5363d48355235e31aa
Sam Ansmink <samansmink@hotmail.com>
2022-01-19 19:06:16 +0100

    refactor: moved verifyS3Helper to tests, changed to camelCase

--- packages/duckdb-wasm/test/httpfs_test.ts
+++ packages/duckdb-wasm/test/httpfs_test.ts
@@ -1,7 +1,66 @@
 import * as duckdb from '../src/';
-import { verifyS3Helper } from '../src/utils'
+import {S3Params, createS3Headers, uriEncode} from '../src/utils'
 import {DuckDBBindings, DuckDBBindingsBase, DuckDBModule} from "../src/";
 import BROWSER_RUNTIME from "../src/bindings/runtime_browser";
+
+// this computes the signature from https://czak.pl/2015/09/15/s3-rest-api-with-curl.html
+const verifyS3Helper = function () {
+    const testParams1 : S3Params = {
+        url: "/",
+        query: "",
+        host: "my-precious-bucket.s3.amazonaws.com",
+        region: "us-east-1",
+        service: "s3",
+        method: "GET",
+        accessKeyId: "AKIAIOSFODNN7EXAMPLE",
+        secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+        sessionToken: "",
+        dateNow: "20150915",
+        datetimeNow: "20150915T124500Z",
+    };
+
+    const test_header = createS3Headers(testParams1);
+    if (test_header.get("Authorization") != "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20150915/us-east-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=182072eb53d85c36b2d791a1fa46a12d23454ec1e921b02075c23aee40166d5a") {
+        throw new Error("Auth header incorrect");
+    }
+
+    const canonical_query_string = "delimiter=%2F&encoding-type=url&list-type=2&prefix="; // aws s3 ls <bucket>
+
+    const testParams2 : S3Params = {
+        url: "/",
+        query: canonical_query_string,
+        host: "my-precious-bucket.s3.eu-west-1.amazonaws.com",
+        region: "eu-west-1",
+        service: "s3",
+        method: "GET",
+        accessKeyId: "ASIAYSPIOYDTHTBIITVC",
+        secretAccessKey: "vs1BZPxSL2qVARBSg5vCMKJsavCoEPlo/HSHRaVe",
+        sessionToken: "IQoJb3JpZ2luX2VjENX//////////wEaCWV1LXdlc3QtMSJHMEUCIQDfjzs9BYHrEXDMU/NR+PHV1uSTr7CSVSQdjKSfiPRLdgIgCCztF0VMbi9+uHHAfBVKhV4t9MlUrQg3VAOIsLxrWyoqlAIIHRAAGgw1ODk0MzQ4OTY2MTQiDOGl2DsYxENcKCbh+irxARe91faI+hwUhT60sMGRFg0GWefKnPclH4uRFzczrDOcJlAAaQRJ7KOsT8BrJlrY1jSgjkO7PkVjPp92vi6lJX77bg99MkUTJActiOKmd84XvAE5bFc/jFbqechtBjXzopAPkKsGuaqAhCenXnFt6cwq+LZikv/NJGVw7TRphLV+Aq9PSL9XwdzIgsW2qXwe1c3rxDNj53yStRZHVggdxJ0OgHx5v040c98gFphzSULHyg0OY6wmCMTYcswpb4kO2IIi6AiD9cY25TlwPKRKPi5CdBsTPnyTeW62u7PvwK0fTSy4ZuJUuGKQnH2cKmCXquEwoOHEiQY6nQH9fzY/EDGHMRxWWhxu0HiqIfsuFqC7GS0p0ToKQE+pzNsvVwMjZc+KILIDDQpdCWRIwu53I5PZy2Cvk+3y4XLvdZKQCsAKqeOc4c94UAS4NmUT7mCDOuRV0cLBVM8F0JYBGrUxyI+YoIvHhQWmnRLuKgTb5PkF7ZWrXBHFWG5/tZDOvBbbaCWTlRCL9b0Vpg5+BM/81xd8jChP4w83",
+        dateNow: "20210904",
+        datetimeNow: "20210904T121746Z",
+    };
+    const test_header2 = createS3Headers(testParams2);
+
+    if (test_header2.get("Authorization") !=
+        "AWS4-HMAC-SHA256 Credential=ASIAYSPIOYDTHTBIITVC/20210904/eu-west-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=4d9d6b59d7836b6485f6ad822de97be40287da30347d83042ea7fbed530dc4c0") {
+        console.log(test_header2.get("Authorization"));
+        console.log("AWS4-HMAC-SHA256 Credential=ASIAYSPIOYDTHTBIITVC/20210904/eu-west-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=4d9d6b59d7836b6485f6ad822de97be40287da30347d83042ea7fbed530dc4c0");
+        throw new Error("test_header2 incorrect");
+    }
+
+    if (uriEncode("/category=Books/") != "/category%3DBooks/") {
+        console.log(uriEncode("/category=Books/"));
+        throw new Error("test fail1");
+    }
+    if (uriEncode("/?category=Books&title=Ducks Retreat/") != "/%3Fcategory%3DBooks%26title%3DDucks%20Retreat/") {
+        throw new Error("test fail2");
+    }
+
+    if (uriEncode("/?category=Books&title=Ducks Retreat/", true) !=
+        "%2F%3Fcategory%3DBooks%26title%3DDucks%20Retreat%2F") {
+        throw new Error("test fail3");
+    }
+}
 
 export function testHTTPFS(adb: () => duckdb.AsyncDuckDB, sdb: () => duckdb.DuckDBBindings): void {
     let conn_async: duckdb.AsyncDuckDBConnection | null;
