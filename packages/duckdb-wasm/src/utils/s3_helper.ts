a961304b9c5b239615f1df927d57e5d980196882
Sam Ansmink <samansmink@hotmail.com>
2022-01-19 19:06:16 +0100

    refactor: moved verifyS3Helper to tests, changed to camelCase

--- packages/duckdb-wasm/src/utils/s3_helper.ts
+++ packages/duckdb-wasm/src/utils/s3_helper.ts
@@ -1,18 +1,18 @@
 import {S3Config} from "../bindings";
 import {sha256} from "js-sha256";
 
-interface S3Params {
+export interface S3Params {
     url: string,
     query: string,
     host: string,
     region: string,
     service: string,
     method: string,
-    access_key_id: string,
-    secret_access_key: string,
-    session_token: string,
-    date_now: string,
-    datetime_now: string
+    accessKeyId: string,
+    secretAccessKey: string,
+    sessionToken: string,
+    dateNow: string,
+    datetimeNow: string
 }
 
 const getS3Params = function(config : S3Config | undefined, url: string, method : string) : S3Params {
@@ -24,18 +24,18 @@ const getS3Params = function(config : S3Config | undefined, url: string, method 
         region: (config?.region) ?? "",
         service: "s3",
         method: method,
-        access_key_id: (config?.accessKeyId) ?? "",
-        secret_access_key: (config?.secretAccessKey) ?? "",
-        session_token: (config?.sessionToken) ?? "",
-        date_now: new Date().toISOString().replace(/-/g,'').split('T')[0],
-        datetime_now: new Date().toISOString().replace(/-/g,'').replace(/:/g,'').split('.')[0]+ 'Z',
+        accessKeyId: (config?.accessKeyId) ?? "",
+        secretAccessKey: (config?.secretAccessKey) ?? "",
+        sessionToken: (config?.sessionToken) ?? "",
+        dateNow: new Date().toISOString().replace(/-/g,'').split('T')[0],
+        datetimeNow: new Date().toISOString().replace(/-/g,'').replace(/:/g,'').split('.')[0]+ 'Z',
     };
 };
 
-const uri_encode = function(input : string, encode_slash = false) {
+export function uriEncode(input : string, encode_slash = false) {
     // https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
 
-    const hex_digit = "0123456789ABCDEF";
+    const hexDigit = "0123456789ABCDEF";
     let result = "";
 
     for (let i = 0; i < input.length; i++) {
@@ -52,75 +52,73 @@ const uri_encode = function(input : string, encode_slash = false) {
             }
         } else {
             result += "%";
-            result += hex_digit[ch.charCodeAt(0) >> 4];
-            result += hex_digit[ch.charCodeAt(0) & 15];
+            result += hexDigit[ch.charCodeAt(0) >> 4];
+            result += hexDigit[ch.charCodeAt(0) & 15];
         }
     }
     return result;
 }
 
-const createS3Headers = function (params: S3Params) : Map<string, string> {
+const createS3HeadersFromS3Config = function (config : S3Config | undefined, url : string, method : string) : Map<string, string> {
+    const params = getS3Params(config, url, method);
+    return createS3Headers(params);
+}
+
+export function createS3Headers(params: S3Params) : Map<string, string> {
     // this is the sha256 of the empty string, its useful since we have no payload for GET requests
-    const empty_payload_hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    const payloadHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
     const res = new Map<string, string>();
     // res.set("host", params.host)
-    res.set("x-amz-date", params.datetime_now);
-    res.set("x-amz-content-sha256", empty_payload_hash);
+    res.set("x-amz-date", params.datetimeNow);
+    res.set("x-amz-content-sha256", payloadHash);
 
-    if (params.session_token) {
-        res.set("x-amz-security-token", params.session_token);
+    if (params.sessionToken) {
+        res.set("x-amz-security-token", params.sessionToken);
     }
 
     // construct string to sign
-    // hash_bytes canonical_request_hash;
-    // hash_str canonical_request_hash_str;
-    let signed_headers = "host;x-amz-content-sha256;x-amz-date";
-    if (params.session_token) {
-        signed_headers += ";x-amz-security-token";
+    let signedHeaders = "host;x-amz-content-sha256;x-amz-date";
+    if (params.sessionToken) {
+        signedHeaders += ";x-amz-security-token";
     }
 
-    let canonical_request = params.method + "\n" + uri_encode(params.url) + "\n" + params.query + "\nhost:" + params.host +
-        "\nx-amz-content-sha256:" + empty_payload_hash + "\nx-amz-date:" + params.datetime_now;
-    if (params.session_token && params.session_token.length > 0) {
-        canonical_request += "\nx-amz-security-token:" + params.session_token;
+    let canonicalRequest = params.method + "\n" + uriEncode(params.url) + "\n" + params.query + "\nhost:" + params.host +
+        "\nx-amz-content-sha256:" + payloadHash + "\nx-amz-date:" + params.datetimeNow;
+    if (params.sessionToken && params.sessionToken.length > 0) {
+        canonicalRequest += "\nx-amz-security-token:" + params.sessionToken;
     }
 
-    canonical_request += "\n\n" + signed_headers + "\n" + empty_payload_hash;
-    const canonical_request_hash_str = sha256(canonical_request);
+    canonicalRequest += "\n\n" + signedHeaders + "\n" + payloadHash;
+    const canonicalRequestHashStr = sha256(canonicalRequest);
 
-    const string_to_sign = "AWS4-HMAC-SHA256\n" + params.datetime_now + "\n" + params.date_now + "/" + params.region + "/" + params.service +
-        "/aws4_request\n" + canonical_request_hash_str;
+    const stringToSign = "AWS4-HMAC-SHA256\n" + params.datetimeNow + "\n" + params.dateNow + "/" + params.region + "/" + params.service +
+        "/aws4_request\n" + canonicalRequestHashStr;
 
     // ts-ignore's because library can accept array buffer as key, but TS arg is incorrect
-    const sign_key = "AWS4" + params.secret_access_key;
-    const k_date = sha256.hmac.arrayBuffer(sign_key, params.date_now);
+    const signKey = "AWS4" + params.secretAccessKey;
+    const kDate = sha256.hmac.arrayBuffer(signKey, params.dateNow);
 
     // Note, js-sha256 has a bug in the TS interface that only supports strings as keys, while we need a bytearray
     // as key. PR is open but unmerged: https://github.com/emn178/js-sha256/pull/25
     // eslint-disable-next-line
     // @ts-ignore
-    const k_region = sha256.hmac.arrayBuffer(k_date, params.region);
+    const kRegion = sha256.hmac.arrayBuffer(kDate, params.region);
     // eslint-disable-next-line
     // @ts-ignore
-    const k_service = sha256.hmac.arrayBuffer(k_region, params.service,);
+    const kService = sha256.hmac.arrayBuffer(kRegion, params.service,);
     // eslint-disable-next-line
     // @ts-ignore
-    const signing_key = sha256.hmac.arrayBuffer(k_service, "aws4_request");
+    const signingKey = sha256.hmac.arrayBuffer(kService, "aws4_request");
     // eslint-disable-next-line
     // @ts-ignore
-    const signature = sha256.hmac(signing_key, string_to_sign);
+    const signature = sha256.hmac(signingKey, stringToSign);
 
-    res.set("Authorization", "AWS4-HMAC-SHA256 Credential=" + params.access_key_id + "/" + params.date_now + "/" + params.region + "/" +
-        params.service + "/aws4_request, SignedHeaders=" + signed_headers +
+    res.set("Authorization", "AWS4-HMAC-SHA256 Credential=" + params.accessKeyId + "/" + params.dateNow + "/" + params.region + "/" +
+        params.service + "/aws4_request, SignedHeaders=" + signedHeaders +
         ", Signature=" + signature);
 
     return res;
-}
-
-const createS3HeadersFromS3Config = function (config : S3Config | undefined, url : string, method : string) : Map<string, string> {
-    const params = getS3Params(config, url, method);
-    return createS3Headers(params);
 }
 
 export function addS3Headers(xhr: XMLHttpRequest, config : S3Config | undefined, url : string, method: string) {
@@ -136,17 +134,17 @@ export function parseS3Url (url: string) : {bucket : string, path : string} {
     if (url.indexOf("s3://") != 0) {
         throw new Error("URL needs to start with s3://");
     }
-    const slash_pos = url.indexOf('/', 5);
+    const slashPos = url.indexOf('/', 5);
 
-    if (slash_pos == -1) {
+    if (slashPos == -1) {
         throw new Error("URL needs to contain a '/' after the host");
     }
 
-    const bucket = url.substring(5, slash_pos);
+    const bucket = url.substring(5, slashPos);
     if (!bucket) {
         throw new Error("URL needs to contain a bucket name");
     }
-    const path = url.substring(slash_pos);
+    const path = url.substring(slashPos);
     if (!path) {
         throw new Error("URL needs to contain key");
     }
@@ -157,65 +155,6 @@ export function parseS3Url (url: string) : {bucket : string, path : string} {
 export function getHTTPUrl(config : S3Config | undefined, url : string) : string {
     const parsedUrl = parseS3Url(url);
     const host = parsedUrl.bucket + "." + (config?.endpoint ? config?.endpoint : "s3.amazonaws.com");
-    const http_host = "https://" + host;
-    return http_host + parsedUrl.path;
-}
-
-// this computes the signature from https://czak.pl/2015/09/15/s3-rest-api-with-curl.html
-export function verifyS3Helper() {
-    const testParams1 : S3Params = {
-        url: "/",
-        query: "",
-        host: "my-precious-bucket.s3.amazonaws.com",
-        region: "us-east-1",
-        service: "s3",
-        method: "GET",
-        access_key_id: "AKIAIOSFODNN7EXAMPLE",
-        secret_access_key: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-        session_token: "",
-        date_now: "20150915",
-        datetime_now: "20150915T124500Z",
-    };
-
-    const test_header = createS3Headers(testParams1);
-    if (test_header.get("Authorization") != "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20150915/us-east-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=182072eb53d85c36b2d791a1fa46a12d23454ec1e921b02075c23aee40166d5a") {
-        throw new Error("Auth header incorrect");
-    }
-
-    const canonical_query_string = "delimiter=%2F&encoding-type=url&list-type=2&prefix="; // aws s3 ls <bucket>
-
-    const testParams2 : S3Params = {
-        url: "/",
-        query: canonical_query_string,
-        host: "my-precious-bucket.s3.eu-west-1.amazonaws.com",
-        region: "eu-west-1",
-        service: "s3",
-        method: "GET",
-        access_key_id: "ASIAYSPIOYDTHTBIITVC",
-        secret_access_key: "vs1BZPxSL2qVARBSg5vCMKJsavCoEPlo/HSHRaVe",
-        session_token: "IQoJb3JpZ2luX2VjENX//////////wEaCWV1LXdlc3QtMSJHMEUCIQDfjzs9BYHrEXDMU/NR+PHV1uSTr7CSVSQdjKSfiPRLdgIgCCztF0VMbi9+uHHAfBVKhV4t9MlUrQg3VAOIsLxrWyoqlAIIHRAAGgw1ODk0MzQ4OTY2MTQiDOGl2DsYxENcKCbh+irxARe91faI+hwUhT60sMGRFg0GWefKnPclH4uRFzczrDOcJlAAaQRJ7KOsT8BrJlrY1jSgjkO7PkVjPp92vi6lJX77bg99MkUTJActiOKmd84XvAE5bFc/jFbqechtBjXzopAPkKsGuaqAhCenXnFt6cwq+LZikv/NJGVw7TRphLV+Aq9PSL9XwdzIgsW2qXwe1c3rxDNj53yStRZHVggdxJ0OgHx5v040c98gFphzSULHyg0OY6wmCMTYcswpb4kO2IIi6AiD9cY25TlwPKRKPi5CdBsTPnyTeW62u7PvwK0fTSy4ZuJUuGKQnH2cKmCXquEwoOHEiQY6nQH9fzY/EDGHMRxWWhxu0HiqIfsuFqC7GS0p0ToKQE+pzNsvVwMjZc+KILIDDQpdCWRIwu53I5PZy2Cvk+3y4XLvdZKQCsAKqeOc4c94UAS4NmUT7mCDOuRV0cLBVM8F0JYBGrUxyI+YoIvHhQWmnRLuKgTb5PkF7ZWrXBHFWG5/tZDOvBbbaCWTlRCL9b0Vpg5+BM/81xd8jChP4w83",
-        date_now: "20210904",
-        datetime_now: "20210904T121746Z",
-    };
-    const test_header2 = createS3Headers(testParams2);
-
-    if (test_header2.get("Authorization") !=
-        "AWS4-HMAC-SHA256 Credential=ASIAYSPIOYDTHTBIITVC/20210904/eu-west-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=4d9d6b59d7836b6485f6ad822de97be40287da30347d83042ea7fbed530dc4c0") {
-        console.log(test_header2.get("Authorization"));
-        console.log("AWS4-HMAC-SHA256 Credential=ASIAYSPIOYDTHTBIITVC/20210904/eu-west-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, Signature=4d9d6b59d7836b6485f6ad822de97be40287da30347d83042ea7fbed530dc4c0");
-        throw new Error("test_header2 incorrect");
-    }
-
-    if (uri_encode("/category=Books/") != "/category%3DBooks/") {
-        console.log(uri_encode("/category=Books/"));
-        throw new Error("test fail1");
-    }
-    if (uri_encode("/?category=Books&title=Ducks Retreat/") != "/%3Fcategory%3DBooks%26title%3DDucks%20Retreat/") {
-        throw new Error("test fail2");
-    }
-
-    if (uri_encode("/?category=Books&title=Ducks Retreat/", true) !=
-        "%2F%3Fcategory%3DBooks%26title%3DDucks%20Retreat%2F") {
-        throw new Error("test fail3");
-    }
+    const httpHost = "https://" + host;
+    return httpHost + parsedUrl.path;
 }
