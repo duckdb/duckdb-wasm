d3a2156771c0f3cfe70ce6f4a49b8cb4d3ce301f
Sam Ansmink <samansmink@hotmail.com>
2022-01-21 16:00:03 +0100

    test: use local s3 server for testing

---
+++ packages/duckdb-wasm/karma/s3rver/s3rver.js
@@ -0,0 +1,27 @@
+const S3rver = require('s3rver');
+
+const CORS_CONFIG = "<CORSConfiguration>\n" +
+    "  <CORSRule>\n" +
+    "    <AllowedOrigin>*</AllowedOrigin>\n" +
+    "    <AllowedMethod>PUT</AllowedMethod>\n" +
+    "    <AllowedMethod>GET</AllowedMethod>\n" +
+    "    <AllowedMethod>HEAD</AllowedMethod>\n" +
+    "    <AllowedHeader>*</AllowedHeader>\n" +
+    "  </CORSRule>\n" +
+    "</CORSConfiguration>";
+
+var createS3rver = function (args, config, logger) {
+    const log = logger.create('S3-test-server');
+    log.info('Starting S3 test server on port ' + config.s3rver.port);
+    let instance = new S3rver({
+        port: config.s3rver.port,
+        address: 'localhost',
+        silent: config.s3rver.silent,
+        directory: './../../.tmp/s3rver',
+        configureBuckets: [{name: 'test-bucket', configs:[CORS_CONFIG]}]
+    }).run();
+};
+
+module.exports = {
+    'framework:s3rver': ['factory', createS3rver]
+};
