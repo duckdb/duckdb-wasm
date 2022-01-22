7fe3a98ae504384d0f160ec26f5533a2185a3549
Andre Kohn <kohn.a@outlook.com>
2022-01-22 22:27:35 +0100

    ci: split publish script

---
+++ scripts/npm_publish_shell.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
+
+cd ${PROJECT_ROOT}/packages/duckdb-wasm-shell
+mkdir -p ./dist/img
+cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
+${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
+
+npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
+npm publish --ignore-scripts --access public ${TAG} || true
--- scripts/npm_version.sh
+++ scripts/npm_version.sh
@@ -27,21 +27,3 @@ echo "TAG=${TAG}"
 
 cd ${PROJECT_ROOT}
 node ${PROJECT_ROOT}/scripts/sync_versions.mjs
-
-if [[ "$1" = "publish" ]] ; then
-	cd ${PROJECT_ROOT}/packages/duckdb-wasm
-	mkdir -p ./dist/img
-	cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
-	${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
-
-	npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
-	npm publish --ignore-scripts --access public ${TAG} || true
-
-	cd ${PROJECT_ROOT}/packages/duckdb-wasm-shell
-	mkdir -p ./dist/img
-	cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
-	${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
-
-	npm config set //registry.npmjs.org/:_authToken ${NPM_PUBLISH_TOKEN}
-	npm publish --ignore-scripts --access public ${TAG} || true
-fi
