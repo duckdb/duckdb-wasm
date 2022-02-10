2256ccdd39fb248e1e766bfb2c7565f0ee08d56e
Andre Kohn <kohn.a@outlook.com>
2022-02-10 23:14:11 +0100

    fix: export package version in more bundles

--- packages/duckdb-wasm/src/targets/duckdb-browser-blocking.ts
+++ packages/duckdb-wasm/src/targets/duckdb-browser-blocking.ts
@@ -2,6 +2,7 @@ export * from '../bindings';
 export * from '../log';
 export * from '../platform';
 export * from '../status';
+export * from '../version';
 export { DEFAULT_RUNTIME } from '../bindings/runtime';
 export { BROWSER_RUNTIME } from '../bindings/runtime_browser';
 
