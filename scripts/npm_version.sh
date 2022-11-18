7946739f562639fd71035404399d7f5744596cb3
Andre Kohn <kohn.a@outlook.com>
2022-11-18 14:56:59 +0100

    Revert "Use yarn for versioning"
    This reverts commit 3b811bfdef3a6af6b67b168517810dbec2375fa2.

--- scripts/npm_version.sh
+++ scripts/npm_version.sh
@@ -14,13 +14,13 @@ echo "DEV=${DEV}"
 if [[ "${DEV}" = "0" ]] ; then
 	for PKG in ${PROJECT_ROOT}/packages/* ; do
 		cd ${PKG}
-		yarn version ${VERSION}
+		npm version ${VERSION}
 	done
 else 
 	for PKG in ${PROJECT_ROOT}/packages/* ; do
 		cd ${PKG}
-		yarn version ${VERSION}
-		yarn version prerelease --preid="dev"${DEV}
+		npm version ${VERSION}
+		npm version prerelease --preid="dev"${DEV}
 	done
 fi
 echo "TAG=${TAG}"
