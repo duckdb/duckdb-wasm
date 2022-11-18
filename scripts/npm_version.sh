09ddeb4c9175e624813e7ac8ee8d243f768c46c1
Andre Kohn <kohn.a@outlook.com>
2022-11-18 13:35:43 +0100

    Fix yarn versioning script

--- scripts/npm_version.sh
+++ scripts/npm_version.sh
@@ -14,13 +14,13 @@ echo "DEV=${DEV}"
 if [[ "${DEV}" = "0" ]] ; then
 	for PKG in ${PROJECT_ROOT}/packages/* ; do
 		cd ${PKG}
-		yarn version ${VERSION}
+		yarn version --new-version ${VERSION}
 	done
 else 
 	for PKG in ${PROJECT_ROOT}/packages/* ; do
 		cd ${PKG}
-		yarn version ${VERSION}
-		yarn version prerelease --preid="dev"${DEV}
+		yarn version --new-version ${VERSION}
+		yarn version --prerelease --preid="dev"${DEV}
 	done
 fi
 echo "TAG=${TAG}"
