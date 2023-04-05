d96299c8ecd842035d6018166733b30beb6e1246
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-04-05 11:17:46 +0200

    Build and publish WIP package

---
+++ scripts/npm_publish_lib_wip.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
+
+cd ${PROJECT_ROOT}/packages/duckdb-wasm
+mkdir -p ./dist/img
+cp ${PROJECT_ROOT}/misc/duckdb.svg ./dist/img/duckdb.svg
+cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
+${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
+
+npm publish --ignore-scripts --access public ${TAG} --tag wip
