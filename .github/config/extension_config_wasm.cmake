acd521b018efdedbacb42f57534d8ae0104431f8
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-09-26 10:12:37 +0200

    Bump DuckDB submodule

--- .github/config/extension_config_wasm.cmake
+++ .github/config/extension_config_wasm.cmake
@@ -17,13 +17,25 @@ duckdb_extension_load(visualizer DONT_LINK)
 
 #duckdb_extension_load(httpfs DONT_LINK)
 
-duckdb_extension_load(substrait
-    LOAD_TESTS DONT_LINK
-    GIT_URL https://github.com/duckdblabs/substrait
-    GIT_TAG 5d621b1d7d16fe86f8b1930870c8e6bf05bcb92a
-    )
+################# SQLITE_SCANNER
+# Static linking on windows does not properly work due to symbol collision
+if (WIN32)
+    set(STATIC_LINK_SQLITE "DONT_LINK")
+else ()
+    set(STATIC_LINK_SQLITE "")
+endif()
+
 duckdb_extension_load(sqlite_scanner
-        DONT_LINK LOAD_TESTS
+        ${STATIC_LINK_SQLITE} LOAD_TESTS
         GIT_URL https://github.com/duckdblabs/sqlite_scanner
-        GIT_TAG 9c38a30be2237456cdcd423d527b96c944158c77
+        GIT_TAG 3443b2999ae1e68a108568fd32145705237a5760
         )
+
+################# SUBSTRAIT
+if (NOT WIN32)
+    duckdb_extension_load(substrait
+            LOAD_TESTS DONT_LINK
+            GIT_URL https://github.com/duckdblabs/substrait
+            GIT_TAG 5d621b1d7d16fe86f8b1930870c8e6bf05bcb92a
+            )
+endif()
