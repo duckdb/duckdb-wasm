6ec472b70a96fae531650ce23e2281ca0e0d6701
Jeff Raymakers <jray@motherduck.com>
2023-09-26 11:54:40 -0700

    esbuild-browser example using coi

--- examples/esbuild-browser/bundle.mjs
+++ examples/esbuild-browser/bundle.mjs
@@ -12,8 +12,35 @@ function printErr(err) {
 
 fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-mvp.wasm'), './duckdb-mvp.wasm', printErr);
 fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-eh.wasm'), './duckdb-eh.wasm', printErr);
+fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-coi.wasm'), './duckdb-coi.wasm', printErr);
 fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-browser-mvp.worker.js'), './duckdb-browser-mvp.worker.js', printErr);
+fs.copyFile(
+    path.resolve(DUCKDB_DIST, 'duckdb-browser-mvp.worker.js.map'),
+    './duckdb-browser-mvp.worker.js.map',
+    printErr,
+);
 fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-browser-eh.worker.js'), './duckdb-browser-eh.worker.js', printErr);
+fs.copyFile(
+    path.resolve(DUCKDB_DIST, 'duckdb-browser-eh.worker.js.map'),
+    './duckdb-browser-eh.worker.js.map',
+    printErr,
+);
+fs.copyFile(path.resolve(DUCKDB_DIST, 'duckdb-browser-coi.worker.js'), './duckdb-browser-coi.worker.js', printErr);
+fs.copyFile(
+    path.resolve(DUCKDB_DIST, 'duckdb-browser-coi.worker.js.map'),
+    './duckdb-browser-coi.worker.js.map',
+    printErr,
+);
+fs.copyFile(
+    path.resolve(DUCKDB_DIST, 'duckdb-browser-coi.pthread.worker.js'),
+    './duckdb-browser-coi.pthread.worker.js',
+    printErr,
+);
+fs.copyFile(
+    path.resolve(DUCKDB_DIST, 'duckdb-browser-coi.pthread.worker.js.map'),
+    './duckdb-browser-coi.pthread.worker.js.map',
+    printErr,
+);
 
 esbuild.build({
     entryPoints: ['./index.ts'],
