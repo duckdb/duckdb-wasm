9c54a755f6b85e708ba07039cc0771ace839da4a
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-11-01 17:24:09 +0100

    Issue 1467: Add test that this works in regular duckdb-wasm

--- packages/duckdb-wasm/test/regression/index.ts
+++ packages/duckdb-wasm/test/regression/index.ts
@@ -4,7 +4,8 @@ import { test334 } from './github_334.test';
 import { test393 } from './github_393.test';
 import { test448 } from './github_448.test';
 import { test470 } from './github_470.test';
-import { test477 } from "./github_477.test";
+import { test477 } from './github_477.test';
+import { test1467 } from './github_1467.test';
 
 export function testRegressionAsync(adb: () => duckdb.AsyncDuckDB): void {
     test332(adb);
@@ -13,4 +14,5 @@ export function testRegressionAsync(adb: () => duckdb.AsyncDuckDB): void {
     test448(adb);
     test470(adb);
     test477(adb);
+    test1467(adb);
 }
