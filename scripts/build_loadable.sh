ae2c0ec31f16cf4fda4bdd08cb271d43ec6b1dda
Yves <yves@motherduck.com>
2023-11-09 16:26:57 -0500

    Update script to depend on globstar

--- scripts/build_loadable.sh
+++ scripts/build_loadable.sh
@@ -8,13 +8,11 @@ PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 
 mkdir -p loadable_extensions
 shopt -s nullglob
-shopt -s globstar
 
-FILES="build/$1/$2/**/*.duckdb_extension"
-for f in $FILES
+for f in `find ./build/$1/$2 -name '*.duckdb_extension'`
 do
         ext=`basename $f .duckdb_extension`
-        echo $ext
+        echo "Building '$ext'..."
         emcc $f -sSIDE_MODULE=1 -o loadable_extensions/$ext.duckdb_extension.wasm -O3
 done
 
