6e7b6031e50e784f99baafd334a321a69ad41d6f
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-01-29 14:31:31 +0100

    Add fail if npm package size is bigger than 150MB
    Partial fix to https://github.com/duckdb/duckdb-wasm/issues/1592

--- scripts/npm_measure_lib.sh
+++ scripts/npm_measure_lib.sh
@@ -9,4 +9,6 @@ cp ${PROJECT_ROOT}/misc/duckdb_wasm.svg ./dist/img/duckdb_wasm.svg
 ${PROJECT_ROOT}/scripts/build_duckdb_badge.sh > ./dist/img/duckdb_version_badge.svg
 
 npm install -g pkg-size
-pkg-size .
+pkg-size . --sizes=size > output
+cat output
+tail -n2 output | grep " MB" | awk '{print ($1 < 150)}' | grep "1" || exit 1
