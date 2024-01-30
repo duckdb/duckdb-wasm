dff00544e61d14d33c5e0a198eb227fa2ed69ee5
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-01-30 09:30:30 +0100

    Fix deployment of tag next

--- scripts/build_duckdb_badge.sh
+++ scripts/build_duckdb_badge.sh
@@ -6,7 +6,6 @@ BADGEGEN=${PROJECT_ROOT}/node_modules/.bin/badge
 cd ${PROJECT_ROOT}/submodules/duckdb
 VERSION=`git describe --tags --abbrev=0 | tr -d "v"`
 DEV=`git describe --tags --long | cut -f2 -d-`
-TAG=''	
 
 BADGE_LABEL_COLOR="#555"
 BADGE_VALUE_COLOR="#007ec6"
