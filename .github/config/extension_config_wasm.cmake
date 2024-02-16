9238b9a2b83a68bd6157eeada3818ebd903382a1
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-02-16 10:53:47 +0100

    Cleanup .github/config/extension_config_wasm.cmake

--- .github/config/extension_config_wasm.cmake
+++ .github/config/extension_config_wasm.cmake
@@ -22,11 +22,8 @@ duckdb_extension_load(sqlite_scanner
         GIT_TAG 078cd16fbd9a4ff96a71c6f5aafc27343e087cbb
         )
 
-################ SUBSTRAIT
-if (NOT WIN32)
-    duckdb_extension_load(substrait
-            LOAD_TESTS DONT_LINK
-            GIT_URL https://github.com/duckdb/substrait
-            GIT_TAG e75ba6a9a43d75d8c72ecc38e6f751fd8e3474a0
-            )
-endif()
+duckdb_extension_load(substrait
+	LOAD_TESTS DONT_LINK
+	GIT_URL https://github.com/duckdb/substrait
+	GIT_TAG e75ba6a9a43d75d8c72ecc38e6f751fd8e3474a0
+	)
