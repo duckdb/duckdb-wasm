62616bcb434d52fcdfc5ee45ca890c576d4d4adf
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-02-26 10:29:52 +0100

    Remove external repository from extensions configurations

--- .github/config/extension_config_wasm.cmake
+++ .github/config/extension_config_wasm.cmake
@@ -15,15 +15,3 @@ duckdb_extension_load(tpcds DONT_LINK)
 duckdb_extension_load(tpch DONT_LINK)
 
 #duckdb_extension_load(httpfs DONT_LINK)
-
-duckdb_extension_load(sqlite_scanner
-        DONT_LINK LOAD_TESTS
-        GIT_URL https://github.com/duckdb/sqlite_scanner
-        GIT_TAG 078cd16fbd9a4ff96a71c6f5aafc27343e087cbb
-        )
-
-duckdb_extension_load(substrait
-	LOAD_TESTS DONT_LINK
-	GIT_URL https://github.com/duckdb/substrait
-	GIT_TAG e75ba6a9a43d75d8c72ecc38e6f751fd8e3474a0
-	)
