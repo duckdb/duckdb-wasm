fd6c8b6466827eafd3f27e38cc080f9997f05ccf
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-03-11 09:29:44 +0100

    script/generate_tpch_duckdb to be independent of local build

--- scripts/generate_tpch_duckdb.sh
+++ scripts/generate_tpch_duckdb.sh
@@ -7,7 +7,7 @@ trap exit SIGINT
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 DUCKDB_DIR="${PROJECT_ROOT}/submodules/duckdb/"
 DUCKDB_BUILD_DIR="${DUCKDB_DIR}/build/Release"
-DUCKDB_SHELL="${DUCKDB_BUILD_DIR}/duckdb"
+DUCKDB_SHELL="duckdb"
 SCALE_FACTOR=${1:-0.01}
 SCALE_FACTOR_DIR=${SCALE_FACTOR/./_}
 TPCH_DIR=${PROJECT_ROOT}/data/tpch
@@ -16,9 +16,6 @@ TPCH_SF_OUT_DUCKDB=${TPCH_SF_OUT}/duckdb
 TPCH_SF_OUT_DUCKDB_DB=${TPCH_SF_OUT_DUCKDB}/db
 DUCKDB_SCRIPT_FILE=${TPCH_SF_OUT_DUCKDB}/script.sql
 
-if [ ! -f ${DUCKDB_SHELL} ]; then
-    ${PROJECT_ROOT}/scripts/build_duckdb_shell.sh
-fi
 chmod +x ${DUCKDB_SHELL}
 mkdir -p ${TPCH_SF_OUT_DUCKDB}
 rm -r ${TPCH_SF_OUT_DUCKDB}
