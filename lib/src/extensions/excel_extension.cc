329a18646ea5644b233206170347bf4c564f2620
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-29 21:25:32 +0200

    Avoid bundling excel

--- lib/src/extensions/excel_extension.cc
+++
@@ -1,5 +0,0 @@
-#include "duckdb/web/extensions/excel_extension.h"
-
-#include "excel_extension.hpp"
-
-extern "C" void duckdb_web_excel_init(duckdb::DuckDB* db) { db->LoadExtension<duckdb::ExcelExtension>(); }
