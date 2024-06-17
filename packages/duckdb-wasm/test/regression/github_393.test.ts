dd888eff08010357d2c467a1a7295c77475a4100
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-17 20:11:33 +0200

    Explicitly use new Date()

--- packages/duckdb-wasm/test/regression/github_393.test.ts
+++ packages/duckdb-wasm/test/regression/github_393.test.ts
@@ -27,7 +27,7 @@ export function test393(db: () => duckdb.AsyncDuckDB): void {
             const resultWithoutCast = await conn.query<{
                 ts: arrow.TimestampMillisecond;
             }>(`SELECT TIMESTAMP '1992-03-22 01:02:03' as ts`);
-            expect(resultWithoutCast.toArray()[0]?.ts).toEqual(new Date(Date.UTC(1992, 2, 22, 1, 2, 3)).getTime());
+            expect(new Date(resultWithoutCast.toArray()[0]?.ts)).toEqual(new Date(Date.UTC(1992, 2, 22, 1, 2, 3)));
 
             await db().open({
                 path: ':memory:',
@@ -39,7 +39,7 @@ export function test393(db: () => duckdb.AsyncDuckDB): void {
             const resultWithCast = await conn.query<{
                 ts: arrow.DateMillisecond;
             }>(`SELECT TIMESTAMP '1992-03-22 01:02:03' as ts`);
-            expect(resultWithCast.toArray()[0]?.ts).toEqual(new Date(Date.UTC(1992, 2, 22, 1, 2, 3)));
+            expect(new Date(resultWithCast.toArray()[0]?.ts)).toEqual(new Date(Date.UTC(1992, 2, 22, 1, 2, 3)));
         });
     });
 }
