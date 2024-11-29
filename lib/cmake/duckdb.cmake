b20347a175e13277cc3f73af473e33169da422c2
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-11-29 15:19:17 +0100

    Remove fts (v1.1.3 compatible) FIX: delete extra commented out rows

--- lib/cmake/duckdb.cmake
+++ lib/cmake/duckdb.cmake
@@ -15,7 +15,7 @@ endif()
 set(DUCKDB_CXX_FLAGS "${DUCKDB_CXX_FLAGS} -Wno-unqualified-std-cast-call -DDUCKDB_DEBUG_NO_SAFETY -DDUCKDB_FROM_DUCKDB_WASM")
 message("DUCKDB_CXX_FLAGS=${DUCKDB_CXX_FLAGS}")
 
-set(DUCKDB_EXTENSIONS "fts;json")
+set(DUCKDB_EXTENSIONS "json")
 # Escape semicolons in DUCKDB_EXTENSIONS before passing to ExternalProject_Add
 string(REPLACE ";" "$<SEMICOLON>" DUCKDB_EXTENSIONS_PACKED "${DUCKDB_EXTENSIONS}")
 
@@ -59,7 +59,6 @@ ExternalProject_Add(
     <INSTALL_DIR>/lib/libduckdb_utf8proc.a
     <INSTALL_DIR>/lib/libduckdb_fastpforlib.a
     <INSTALL_DIR>/lib/libparquet_extension.a
-    <INSTALL_DIR>/lib/libfts_extension.a
     <INSTALL_DIR>/lib/libjson_extension.a)
 
 ExternalProject_Get_Property(duckdb_ep install_dir)
@@ -105,10 +104,6 @@ target_include_directories(
   INTERFACE ${DUCKDB_SOURCE_DIR}/third_party/thrift
   INTERFACE ${DUCKDB_SOURCE_DIR}/third_party/zstd)
 
-add_library(duckdb_fts STATIC IMPORTED)
-set_property(TARGET duckdb_fts PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libfts_extension.a)
-target_include_directories(duckdb_fts INTERFACE ${DUCKDB_SOURCE_DIR}/extension/fts/include)
-
 add_library(duckdb_parquet STATIC IMPORTED)
 set_property(TARGET duckdb_parquet PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libparquet_extension.a)
 target_include_directories(duckdb_parquet INTERFACE ${DUCKDB_SOURCE_DIR}/extension/parquet/include)
@@ -118,6 +113,5 @@ set_property(TARGET duckdb_json PROPERTY IMPORTED_LOCATION ${install_dir}/lib/li
 target_include_directories(duckdb_json INTERFACE ${DUCKDB_SOURCE_DIR}/extension/json/include)
 
 add_dependencies(duckdb duckdb_ep)
-add_dependencies(duckdb_fts duckdb_ep)
 add_dependencies(duckdb_parquet duckdb_ep)
 add_dependencies(duckdb_json duckdb_ep)
--- lib/src/extensions/fts_extension.cc
+++
@@ -1,5 +0,0 @@
-#include "duckdb/web/extensions/fts_extension.h"
-
-#include "fts_extension.hpp"
-
-extern "C" void duckdb_web_fts_init(duckdb::DuckDB* db) { db->LoadExtension<duckdb::FtsExtension>(); }
