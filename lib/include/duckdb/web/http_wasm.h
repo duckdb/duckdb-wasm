86fc5f565a467ea96a88b52fff8f54b50d58dc80
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-09-15 09:33:22 +0200

    Align HTTPParams with duckdb-httpfs one

--- lib/include/duckdb/web/http_wasm.h
+++ lib/include/duckdb/web/http_wasm.h
@@ -10,16 +10,24 @@ class HTTPState;
 struct HTTPFSParams : public HTTPParams {
     HTTPFSParams(HTTPUtil &http_util) : HTTPParams(http_util) {}
 
-    static constexpr bool DEFAULT_ENABLE_SERVER_CERT_VERIFICATION = false;
-    static constexpr uint64_t DEFAULT_HF_MAX_PER_PAGE = 0;
-    static constexpr bool DEFAULT_FORCE_DOWNLOAD = false;
 
-    bool force_download = DEFAULT_FORCE_DOWNLOAD;
-    bool enable_server_cert_verification = DEFAULT_ENABLE_SERVER_CERT_VERIFICATION;
-    idx_t hf_max_per_page = DEFAULT_HF_MAX_PER_PAGE;
-    string ca_cert_file;
-    string bearer_token;
-    shared_ptr<HTTPState> state;
+        static constexpr bool DEFAULT_ENABLE_SERVER_CERT_VERIFICATION = false;
+        static constexpr uint64_t DEFAULT_HF_MAX_PER_PAGE = 0;
+        static constexpr bool DEFAULT_FORCE_DOWNLOAD = false;
+        static constexpr bool AUTO_FALLBACK_TO_FULL_DOWNLOAD = true;
+
+        bool force_download = DEFAULT_FORCE_DOWNLOAD;
+        bool auto_fallback_to_full_download = AUTO_FALLBACK_TO_FULL_DOWNLOAD;
+        bool enable_server_cert_verification = DEFAULT_ENABLE_SERVER_CERT_VERIFICATION;
+        bool enable_curl_server_cert_verification = true;
+        idx_t hf_max_per_page = DEFAULT_HF_MAX_PER_PAGE;
+        string ca_cert_file;
+        string bearer_token;
+        bool unsafe_disable_etag_checks {false};
+        shared_ptr<HTTPState> state;
+
+
+
 };
 
 class HTTPWasmUtil : public HTTPUtil {
